f4c31db31904c7ce27f6dfe21d047142c5575aeb
Matt Gaidica <matt@gaidi.ca>
2015-03-12 13:40:35 -0400

    no line styles

--- optoPETHs.m
+++ optoPETHs.m
@@ -75,12 +75,8 @@ allTsArr = {};
 for ii=1:length(allTs)
     allTsArr{ii} = allTs{1,ii}{1};
 end
-LineFormat = struct();
-LineFormat.Color = [0.3 0.3 0.3];
-LineFormat.LineWidth = 0.1;
-LineFormat.LineStyle = ':';
 figure;
-plotSpikeRaster(allTsArr,'LineFormat',LineFormat,'PlotType','vertline');
+plotSpikeRaster(allTsArr,'PlotType','vertline');
 hold on;
 for ii=1:length(laserOn)
     p = patch([laserOn(ii) laserOff(ii) laserOff(ii) laserOn(ii)],[0 0 length(allTsArr)+1 length(allTsArr)+1],'g');
