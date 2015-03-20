58987685c9363fbd00b31a3ede91568bf9f36cb0
Matt Gaidica <matt@gaidi.ca>
2015-03-19 21:59:14 -0400

    Other events

--- ChoiceTask/tetrodeEventAnalysis.m
+++ ChoiceTask/tetrodeEventAnalysis.m
@@ -20,7 +20,7 @@ function tetrodeEventAnalysis(sessionConf,nexStruct)
     compiledEvents.cueOn = [1,3,5,7,9];
     compiledEvents.noseIn = [17,19,21,23,25];
     compiledEvents.toneOn = [33,35];
-    compiledEvents.noseIn = [18,20,22,24,26];
+    compiledEvents.noseOut = [18,20,22,24,26];
     compiledEvents.foodOn = 13;
     compiledEvents.foodportOn = 27;
     compiledEvents.houselightOn = 11;
