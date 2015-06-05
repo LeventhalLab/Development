2016ffb1864c8bdbbd99147abd71214839d82b40
Matt Gaidica <matt@gaidi.ca>
2015-06-05 10:56:13 -0400

    Optimizing mtspect, random samples looks good

--- ChoiceTask/burstLFPAnalysis.m
+++ ChoiceTask/burstLFPAnalysis.m
@@ -1,14 +1,19 @@
 function burstLFPAnalysis(data,Fs,burstLocs)
-spectHalfWindow = 2; % seconds
+spectHalfWindow = 1; % seconds
 nDownsample = 10;
 fpass = [5 80];
-numfreqs = 50;
+numfreqs = 25;
 
 [b,a] = butter(2, 0.015); % 183Hz lowpass
 data = filtfilt(b,a,double(data)); % filter both ways
 data = downsample(data,nDownsample); % make smaller to run faster
 Fs = Fs / nDownsample;
 burstLocs = round(burstLocs / nDownsample);
+
+movingwin=[0.5 0.05];
+params.fpass = fpass;
+params.tapers = [5 9];
+params.Fs = Fs;
 
 % filename = '/Users/mattgaidica/Documents/Data/ChoiceTask/R0075/R0075-processed/R0075_20150518a/R0075_20150518a_T05_WL48_PL16_DT24-03-hs.nex';
 % tsCell = leventhalNexTs(filename);
@@ -21,7 +26,7 @@ spectHalfSamples = round(spectHalfWindow * Fs);
 
 Wlfp = [];
 burstCount = 1;
-burstLocsSample = sort(datasample(burstLocs,min(length(burstLocs),10000),'Replace',false));
+burstLocsSample = sort(datasample(burstLocs,min(length(burstLocs),25),'Replace',false));
 for ii=1:length(burstLocsSample)
     % skip if near beginning or end of recording
     if ~(burstLocsSample(ii) > spectHalfSamples * 2 || burstLocsSample(ii) > length(data) - spectHalfSamples * 2)
@@ -30,10 +35,7 @@ for ii=1:length(burstLocsSample)
     % pad with Fs for processing (1 second)
     processRange = (burstLocsSample(ii) - spectHalfSamples + 1) - round(Fs):(burstLocsSample(ii) + spectHalfSamples) + round(Fs);
     %[W,freqList] = calculateComplexScalograms_EnMasse(data(processRange)','Fs',Fs,'fpass',fpass,'numfreqs',numfreqs);
-    movingwin=[0.5 0.05];
-    params.fpass = [0 80];
-    params.tapers = [5 9];
-    params.Fs = Fs;
+    
     [S1,t,f] = mtspecgramc(data(processRange)',movingwin,params);
 %     halfW = size(W,1) / 2;
 %     lfpRange = halfW - spectHalfSamples + 1:halfW + spectHalfSamples;
@@ -61,9 +63,12 @@ end
 % caxis([0 7])
 
 Wavg = squeeze(mean(Wlfp,1));
+halfT = (max(t) - min(t)) / 2;
+t = linspace(-halfT,halfT,size(t,2));
 figure;
 plot_matrix(Wavg,t,f);
 colormap(jet);
 caxis([0 60]);
+xlim([-spectHalfWindow spectHalfWindow]);
 
 disp('end');
