4c093dd499dfe66fd35fcb44db530b4b2fc6c783
Matt Gaidica <matt@gaidi.ca>
2015-06-05 14:22:28 -0400

    Creating feature space for bursts

--- ChoiceTask/burstLFPAnalysis.m
+++ ChoiceTask/burstLFPAnalysis.m
@@ -1,8 +1,7 @@
-function [Wavg,t,f] = burstLFPAnalysis(data,Fs,burstLocs,nAvg,titleText)
+function [Wlfp,t,f] = burstLFPAnalysis(data,Fs,burstLocs)
 spectHalfWindow = 1; % seconds
 nDownsample = 10;
 fpass = [5 80];
-numfreqs = 25;
 
 [b,a] = butter(2, 0.015); % 183Hz lowpass
 data = filtfilt(b,a,double(data)); % filter both ways
@@ -19,59 +18,27 @@ spectHalfSamples = round(spectHalfWindow * Fs);
 
 Wlfp = [];
 burstCount = 1;
-burstLocsSample = sort(datasample(burstLocs,min(length(burstLocs),nAvg),'Replace',false));
 h = waitbar(0,'Processing...');
-for ii=1:length(burstLocsSample)
+for ii=1:length(burstLocs)
     % skip if near beginning or end of recording
-    if (burstLocsSample(ii) < spectHalfSamples * 2 || burstLocsSample(ii) > length(data) - spectHalfSamples * 2)
-        disp(['Skipping burst location ',num2str(burstLocsSample(ii))]);
+    if (burstLocs(ii) < spectHalfSamples * 2 || burstLocs(ii) > length(data) - spectHalfSamples * 2)
+        disp(['Skipping burst location ',num2str(burstLocs(ii))]);
         continue;
     end
     % pad with Fs for processing (1 second)
-    processRange = (burstLocsSample(ii) - spectHalfSamples + 1) - round(Fs):(burstLocsSample(ii) + spectHalfSamples) + round(Fs);
-    %[W,freqList] = calculateComplexScalograms_EnMasse(data(processRange)','Fs',Fs,'fpass',fpass,'numfreqs',numfreqs);
+    processRange = (burstLocs(ii) - spectHalfSamples + 1) - round(Fs):(burstLocs(ii) + spectHalfSamples) + round(Fs);
     
     [S1,t,f] = mtspecgramc(data(processRange)',movingwin,params);
-%     halfW = size(W,1) / 2;
-%     lfpRange = halfW - spectHalfSamples + 1:halfW + spectHalfSamples;
-%     Wlfp(burstCount,:,:) = W(lfpRange,1,:);
     Wlfp(burstCount,:,:) = S1(:,:);
     burstCount = burstCount + 1;
-    
-%     figure;
-%     plot_matrix(S1,t,f);
-%     colormap(jet);
-%     caxis([0 60]);
+
     progress = max(processRange)/length(data);
-%     disp(num2str(progress));
     h = waitbar(progress,h,'Processing...');
 end
 
 close(h);
-
-% Wavg = squeeze(abs(mean(Wlfp,1)).^2)';
-% t = linspace(-spectHalfWindow,spectHalfWindow,size(Wavg,2));
-% 
-% figure;
-% imagesc(t, freqList, log(Wavg));
-% ylabel('Frequency (Hz)');
-% xlabel('Time (s)');
-% set(gca, 'YDir', 'normal');
-% colormap(jet);
-% caxis([0 7])
-
-Wavg = squeeze(mean(Wlfp,1));
-Wavg = 10*log10(Wavg);
 halfT = (max(t) - min(t)) / 2;
 t = linspace(-halfT,halfT,size(t,2));
 
-% figure;
-% imagesc(t,f,Wavg');
-% axis xy; 
-% colorbar;
-% title(titleText);
-% colormap(jet);
-% caxis([0 60]);
-% xlim([-spectHalfWindow spectHalfWindow]);
 
 % disp('end');
