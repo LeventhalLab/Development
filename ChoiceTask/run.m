bb62bc7267d88752927e403a34fc69438765a758
Matt Gaidica <matt@gaidi.ca>
2015-06-11 09:06:32 -0400

    Testing different filters

--- ChoiceTask/run.m
+++ ChoiceTask/run.m
@@ -1,28 +1,20 @@
-% sevFile = '/Users/mattgaidica/Documents/Data/ChoiceTask/R0075/R0075-rawdata/R0075_20150518a/R0075_20150518a/Unnamed_data_ch7.sev';
-% [sev,header] = read_tdt_sev(sevFile);
-% [b,a] = butter(4, [0.02 0.2]);
-% fdata = filtfilt(b,a,double(sev));
-% 
-% nexFile = '/Users/mattgaidica/Documents/Data/ChoiceTask/R0075/R0075-processed/R0075_20150518a/R0075_20150518a_T02_WL48_PL16_DT24.nex';
-% tsCell = leventhalNexTs(nexFile);
-% 
-% ts = tsCell{1,2};
-% [burstEpochs,burstFreqs] = findBursts(ts);
-
-burstIdxs = burstEpochs(burstFreqs >= 191,:);
-slowIdxs = burstEpochs(burstFreqs < 191,:);
-% randomLocs = sort(datasample(min(burstLocs):max(burstLocs),length(burstLocs),'Replace',false));
-
-figure('position',[100 100 800 400]);
-
+figure;
 hold on;
-for ii=1:10:500
-    plot(fdata(header.Fs*ts(slowIdxs(ii,1))-header.Fs/1000:header.Fs*ts(slowIdxs(ii,2))+header.Fs/1000),'r');
-end
+plot(data);
+butterdata = filtfilt(b,a,double(data));
+% [smoothdata] = eegfilt(sev(1:1e6),header.Fs,500,2500);
+% plot(smoothdata);
+% [smoothdata] = eegfilt(sev(1:1e6),header.Fs,750,2500);
+% plot(smoothdata);
+% [smoothdata] = eegfilt(sev(1:1e6),header.Fs,1000,2500);
+% plot(smoothdata);
 
-hold on;
-for ii=1:10:500
-    plot(fdata(header.Fs*ts(burstIdxs(ii,1))-header.Fs/1000:header.Fs*ts(burstIdxs(ii,2))+header.Fs/1000),'b');
-end
+% [smoothdata] = eegfilt(sev(1:1e6),header.Fs,750,2000);
+% plot(smoothdata);
+[smoothdata] = eegfilt(sev(1:1e6),header.Fs,750,3000);
+plot(smoothdata);
+plot(butterdata);
+% [smoothdata] = eegfilt(sev(1:1e6),header.Fs,750,4000);
+% plot(smoothdata);
 
-grid on;
+% legend({'500,2500','750,2500','1000,2500','500,4000','500,5000','500,7500'});
