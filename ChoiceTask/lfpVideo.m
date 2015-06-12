4c88c7e28f9063efb6e1083064cba7e7d8fc0bcc
Matt Gaidica <matt@gaidi.ca>
2015-06-12 12:00:05 -0400

    Videos being created, needs optimization

--- ChoiceTask/lfpVideo.m
+++ ChoiceTask/lfpVideo.m
@@ -6,60 +6,87 @@ function lfpVideo(sessionConf,nexData,lfpChannels)
         error('novideo');
     end
     video = VideoReader(fullfile(leventhalPaths.rawdata,videos(1).name));
+    
+    % setup new video
+    lfpVideoPath = fullfile(leventhalPaths.graphs,'lfpVideos');
+    if ~isdir(lfpVideoPath)
+        mkdir(lfpVideoPath);
+    end
+    saveVideoAs = fullfile(lfpVideoPath,[sessionConf.sessionName,'_',strrep(num2str(lfpChannels),'  ','-')]);
+    newVideo = VideoWriter(saveVideoAs,'Motion JPEG AVI');
+    newVideo.Quality = 100;
+    newVideo.FrameRate = video.FrameRate;
+    open(newVideo);
 
     plotHalfWidthSec = 4; % seconds
-    plotHalfWidthSample = plotHalfWidthSec * sessionConf.Fs;
+    plotHalfWidthSample = round(plotHalfWidthSec * sessionConf.Fs);
     behaviorStartTime = getBehaviorStartTime(nexData);
     iFrameStart = ceil((plotHalfWidthSec + behaviorStartTime) * video.FrameRate);
     
     fullSevFiles = getChFileMap(leventhalPaths.channels);
     
-    figureHeight = 800; % pixels
     videoDimDivider = 3;
-    figure('Position',[0 0 ceil(video.Width/videoDimDivider) figureHeight]);
-    for iFrame=iFrameStart:video.NumberOfFrames
+    figureHeight = 800; % pixels
+    subplotHeight = (figureHeight - ceil(video.Height/videoDimDivider)) / length(lfpChannels);
+    h = figure('Position',[0 0 ceil(video.Width/videoDimDivider) figureHeight]);
+    
+    hicutoff = 500;
+    decimateFactor = floor((sessionConf.Fs/2) / hicutoff); % optimized
+    dFs = sessionConf.Fs / decimateFactor;
+    movingwin=[0.5 0.05];
+    params.fpass = [0 80];
+    params.tapers = [5 9];
+    params.Fs = dFs;
+    
+    smoothdata = [];
+    for iCh = 1:length(lfpChannels)
+        [sev,header] = read_tdt_sev(fullSevFiles{lfpChannels(iCh)});
+        sev = decimate(double(sev(1:1e6)),decimateFactor);
+        smoothdata(iCh,:) = eegfilt(sev,header.Fs,[],hicutoff); % lowpass
+    end
+        
+    for iFrame=iFrameStart:iFrameStart+10%video.NumberOfFrames
         curEphysTs = (1/video.FrameRate) * iFrame - behaviorStartTime;
-        curEphysSample = round(curEphysTs * sessionConf.Fs);
+        curEphysSample = round(curEphysTs * dFs);
+        sampleRange = curEphysSample - round(plotHalfWidthSample/decimateFactor) + 1:curEphysSample + round(plotHalfWidthSample/decimateFactor);
         frame = read(video,iFrame);
         hVid = subplot(length(lfpChannels)+1,1,1);
         set(hVid,'Units','pixels');
         imshow(frame,'border','tight');
         
         hs = []; % subplot handles
-        f = 1:80;
-        hicutoff = 500;
-        decmiateFactor = 10;% floor((sessionConf.Fs/2) / hicutoff);
-        window = round((sessionConf.Fs/decmiateFactor)*.1);
-        overlap = [];
-%         overlap = window/10;
         for iCh = 1:length(lfpChannels)
-            [sev,header] = read_tdt_sev(fullSevFiles{lfpChannels(iCh)});
-            data = sev(curEphysSample-plotHalfWidthSample+1:curEphysSample+plotHalfWidthSample);
-            smoothdata = eegfilt(data,header.Fs,[],hicutoff); % lowpass
-            smoothdata = decimate(smoothdata,decmiateFactor);
-            [s,f,t] = spectrogram(smoothdata,window,overlap,f,header.Fs/decmiateFactor);
-            sLog = 10*log10(abs(s));
             hs(iCh) = subplot(length(lfpChannels)+1,1,iCh+1);
             
-            
-%             movingwin=[0.5 0.05];
-%             params.fpass = [0 80];
-%             params.tapers = [5 9];
-%             params.Fs = sessionConf.Fs/decmiateFactor;
-%             [S1,t,f] = mtspecgramc(smoothdata',movingwin,params);
-%             sLog = 10*log10(abs(S1));
-%             
-            imagesc(t,f,sLog);
+            [S1,t,f] = mtspecgramc(smoothdata(iCh,sampleRange)',movingwin,params);
+            sLog = 10*log10(abs(S1));
+            tAdj = t - plotHalfWidthSec;
+          
+            imagesc(tAdj,f,sLog');
+            hold on;
+            plot([0 0],[min(f) max(f)],'k','LineWidth',2);
             set(gca,'YDir','normal');
             axis xy; 
             axis tight;
             colormap('jet');
+            caxis([-4.5 65]);
+            ticklabelinside(hs(iCh));
+        end
+        
+        for iCh = 1:length(lfpChannels)
+            set(hs(iCh),'Units','pixels');
+            bottomPos = (length(lfpChannels) - iCh) * subplotHeight;
+            set(hs(iCh),'Position',[1 bottomPos ceil(video.Width/videoDimDivider) subplotHeight]);
         end
         
         set(hVid,'Position',[1 figureHeight-ceil(video.Height/videoDimDivider)...
             ceil(video.Width/videoDimDivider) ceil(video.Height/videoDimDivider)]);
-        disp('here');
+        
+        figFrame = getframe(h);
+        writeVideo(newVideo,figFrame);
     end
+    
+    close(newVideo);
 end
 
 function h = subplottight(n,m,i)
