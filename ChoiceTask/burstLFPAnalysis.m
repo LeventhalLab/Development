d746ea37e7576e32d2a8bcdeafc4c7815f21f32e
Matt Gaidica <matt@gaidi.ca>
2015-11-04 21:41:57 -0500

    Pass in filtered data now

--- ChoiceTask/burstLFPAnalysis.m
+++ ChoiceTask/burstLFPAnalysis.m
@@ -1,14 +1,6 @@
 function [Wlfp,t,f,validBursts] = burstLFPAnalysis(data,Fs,burstLocs)
 spectHalfWindow = 4; % seconds
-nDownsample = 10;
 fpass = [0 80];
-
-[b,a] = butter(2, 0.015); % 183Hz lowpass
-data = artifactThresh(data,1,500);
-data = filtfilt(b,a,double(data)); % filter both ways
-data = downsample(data,nDownsample); % make smaller to run faster
-Fs = Fs / nDownsample;
-burstLocs = round(burstLocs / nDownsample);
 
 movingwin=[1 0.05];
 params.fpass = fpass;
@@ -33,13 +25,16 @@ for ii=1:length(burstLocs)
     % pad with Fs for processing (1 second)
     processRange = (burstLocs(ii) - spectHalfSamples + 1) - round(Fs):(burstLocs(ii) + spectHalfSamples) + round(Fs);
     
-    [S1,t,f] = mtspecgramc(data(processRange)',movingwin,params);
+    [S1,t,f] = mtspecgramc(data(round(processRange))',movingwin,params);
+    
+    figure; plot_matrix(S1,t,f); %debug
+    
     Wlfp(burstCount,:,:) = S1(:,:);
     validBursts(ii) = 1;
     burstCount = burstCount + 1;
 
     progress = max(processRange)/length(data);
-    h = waitbar(progress,h,['Processing ',num2str(burstCount-1),'/',num2str(length(burstLocs))]);
+    waitbar(progress,h,['Processing ',num2str(burstCount-1),'/',num2str(length(burstLocs))]);
 end
 
 close(h);
--- ChoiceTask/plotBurstLFPs.m
+++ ChoiceTask/plotBurstLFPs.m
@@ -1,7 +1,9 @@
 function plotBurstLFPs(sessionConf)
+    %[] should do burst detection first and save results, then process LFPs
     oldFs = 24414;
     newFs = 24414.0625;
-    nLocs = 400;
+    nLocs = 10; % old was 400
+    nDownsample = 10;
     
     leventhalPaths = buildLeventhalPaths(sessionConf);
     matFiles = dir(fullfile(leventhalPaths.finished,'*.mat'));
@@ -12,8 +14,16 @@ function plotBurstLFPs(sessionConf)
         % load the nexStruct (first file)
         load(fullfile(leventhalPaths.finished,matFiles(1).name),'nexStruct');
     end
-
+    
+    neuronNames = {};
     for ii=1:length(nexStruct.neurons)
+        neuronNames{ii} = nexStruct.neurons{ii}.name;
+    end
+    neuronIds = listdlg('PromptString','Select neurons:',...
+                'SelectionMode','multiple','ListSize',[200 200],...
+                'ListString',neuronNames);
+
+    for ii=neuronIds
         neuronName = nexStruct.neurons{ii}.name;
         tetrodeName = getTetrodeName(neuronName);
         disp(neuronName);
@@ -25,22 +35,33 @@ function plotBurstLFPs(sessionConf)
         fullSevFiles = getChFileMap(leventhalPaths.channels);
         [sev,header] = read_tdt_sev(fullSevFiles{lfpChannel});
         
+        disp('Scaling data...');
+        [b,a] = butter(2, 0.015); % 183Hz lowpass
+%         data = artifactThresh(sev,1,750);s
+        data = filtfilt(b,a,double(sev)); % filter both ways
+        data = downsample(data,nDownsample); % make smaller to run faster
+        Fs = header.Fs / nDownsample;
+        
         % do burst detection
         ts = adjustTs(nexStruct.neurons{ii,1}.timestamps,oldFs,newFs);
+        %[] save figure?
         [burstEpochs,burstFreqs] = findBursts(ts);
+        disp('Analyzing LFP based on burst locs...');
         burstIdx = burstEpochs(:,1);
-        burstLocs  = ts(burstIdx) * header.Fs;
+        burstLocs = round(ts(burstIdx) * Fs);
+%         burstLocs = round(burstLocs / nDownsample);
         
         randomNospikeLocs = sort(datasample(min(burstLocs):max(burstLocs),length(burstLocs),'Replace',false));
-        randomSpikeLocs = sort(datasample(ts,length(burstLocs),'Replace',false)) * header.Fs;
+        randomSpikeLocs = sort(datasample(ts,length(burstLocs),'Replace',false)) * Fs;
 
 %         figure;plot(burstLocs);hold on;plot(randomNospikeLocs);plot(randomSpikeLocs); %sanity
 %         legend('burstLocs','randomNospike','randomSpike');
-        figure('position',[100 100 300 800]);
+        h = figure('position',[100 100 300 800]);
         randomNospikeLocsSample = sort(randsample(randomNospikeLocs,nLocs));
-        [WlfprandomNospikeLocs,t,f,validBursts] = burstLFPAnalysis(sev,header.Fs,randomNospikeLocsSample);
-        WavgrandomNospikeLocsSample = 10*log10(squeeze(mean(WlfprandomNospikeLocs,1)));
+        [WlfprandomNospikeLocs,t,f,validBursts] = burstLFPAnalysis(data,Fs,randomNospikeLocsSample);
+        WavgrandomNospikeLocsSample = squeeze(mean(WlfprandomNospikeLocs,1));
         
+        figure(h);
         subplot(511);
         %imagesc(t,f,WavgrandomNospikeLocsSample');
         plot_matrix(WavgrandomNospikeLocsSample,t,f);
@@ -48,31 +69,36 @@ function plotBurstLFPs(sessionConf)
         quickFormat('Random No Spike',c);
         
         randomSpikeLocsSample = sort(randsample(randomSpikeLocs,nLocs));
-        [WlfprandomSpikeLocs,t,f,validBursts] = burstLFPAnalysis(sev,header.Fs,randomSpikeLocsSample);
-        WavgrandomSpikeLocsSample = 10*log10(squeeze(mean(WlfprandomSpikeLocs,1)));
+        [WlfprandomSpikeLocs,t,f,validBursts] = burstLFPAnalysis(data,Fs,randomSpikeLocsSample);
+        WavgrandomSpikeLocsSample = squeeze(mean(WlfprandomSpikeLocs,1));
         
+        figure(h);
         subplot(512);
         plot_matrix(WavgrandomSpikeLocsSample,t,f);
         quickFormat('Random Spike',c);
         
         burstLocsSample = sort(randsample(burstLocs,nLocs));
-        [Wlfpbursts,t,f,validBursts] = burstLFPAnalysis(sev,header.Fs,burstLocsSample);
-        WavgburstLocsSample = 10*log10(squeeze(mean(Wlfpbursts,1)));
+        [Wlfpbursts,t,f,validBursts] = burstLFPAnalysis(data,Fs,burstLocsSample);
+        WavgburstLocsSample = squeeze(mean(Wlfpbursts,1));
         
+        figure(h);
         subplot(513);
         plot_matrix(WavgburstLocsSample,t,f);
         quickFormat('Bursts',c);
 
+        figure(h);
         subplot(514);
         plot_matrix(abs(WavgburstLocsSample-WavgrandomNospikeLocsSample),t,f);
         c = caxis;
         quickFormat('(Burst - Random No Spike)',c);
 
+        figure(h);
         subplot(515);
         plot_matrix(abs(WavgburstLocsSample-WavgrandomSpikeLocsSample),t,f);
         quickFormat('(Burst - Random Spike)',c);
         
-        disp('fig');
+        str = {['Neuron: ',neuronName]};
+        annotation('textbox', [.1 .9 .9 .1],'String', str, 'edgeColor','none');
     end
 end
 
