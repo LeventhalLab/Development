d8352e3b40bbf7720ad8718adbbfbd5fcba06d11
Matt Gaidica <matt@gaidi.ca>
2015-12-21 11:25:17 -0500

    Create files en masse

--- ChoiceTask/run.m
+++ ChoiceTask/run.m
@@ -1,2 +1,6 @@
-[meanWaveform, upperStd, lowerStd, ch, windowSize] = aveWaveform(ts_ch37_1, '/Volumes/BOOTCAMP/Users/Matt/Documents/Data/R0088_20151027a/R0088_20151027_R0088_20151027-1_data_ch37.sev');
-plotAveWaveform(meanWaveform, upperStd, lowerStd, ch, windowSize);
+sessionConf = exportSessionConf('R0088_20151028a','nasPath',nasPath);
+createDDTFiles(sessionConf);
+
+
+sessionConf = exportSessionConf('R0088_20151102a','nasPath',nasPath);
+createDDTFiles(sessionConf);
