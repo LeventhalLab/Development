d2ec5daecc9718d2cb7c26169902cf76c5ff792f
Matt Gaidica <matt@gaidi.ca>
2016-01-11 20:41:05 -0500

    Plotting and saving the analysis based on user selected list

--- ChoiceTask/burstEventAnalysis_plot.m
+++ ChoiceTask/burstEventAnalysis_plot.m
@@ -1,14 +1,52 @@
 function burstEventAnalysis_plot(sessionConf)
+    saveRows = 4;
+    burstFiles = rdir(fullfile(sessionConf.nasPath,sessionConf.ratID,[sessionConf.ratID,'-graphs'],'*','burstEventAnalysis','*burstEvents.mat'));
+    plotEventIdx = [1 2 3 4 5 6 8]; % removed foodClick becaue it mirrors SideIn
 
-burstFiles = rdir(fullfile(sessionConf.nasPath,sessionConf.ratID,[sessionConf.ratID,'-graphs'],'*','burstEventAnalysis','*burstEvents.mat'));
+    fileNames = {burstFiles.name};
+    fileIds = listdlg('PromptString','Select neurons:',...
+                    'SelectionMode','multiple','ListSize',[900 700],...
+                    'ListString',fileNames);
 
-for ii=1:length(burstFiles)
-    load(burstFiles(ii).name);
-    if mod(ii,7) == 1
-        h = formatSheet();
+    for iFile=1:length(fileIds)
+        disp([burstFiles(fileIds(iFile)).name]);
+        load(burstFiles(fileIds(iFile)).name);
+        fontSize = 6;
+        if mod(iFile,saveRows) == 1
+            if exist('fig','var')
+                saveFigure(sessionConf,fig);
+                clear fig;
+            end
+            fig = formatSheet();
+            iSubplot = 1;
+        end
+
+        for iEvent=1:length(plotEventIdx)
+            iField = plotEventIdx(iEvent);
+            subplot(saveRows,7,iSubplot);
+            [tsCounts,tsCenters] = hist(pethCell{iField,1},histBin);
+            [tsBurstCounts,~] = hist(pethCell{iField,2},histBin);
+            ax = plotyy(tsCenters,(tsCounts/spikesPerSecondFactor)*histBin,tsCenters,(tsBurstCounts/spikesPerSecondFactor)*histBin);
+            set(ax,'FontSize',fontSize);
+            xlabel('Time (s)','FontSize',fontSize);
+            ylabel('Spikes/Second','FontSize',fontSize);
+            title({strrep(neuronName,'_','-'),eventFieldnames{iField}},'FontSize',fontSize);
+            if iFile==1 && iEvent==1
+                legend('all spikes','burst spikes','location','northwest','FontSize',fontSize);
+            end
+            iSubplot = iSubplot + 1;
+        end
     end
     
-    for iEvent=[1 2 3 4 5 6 8];
-        subplot(7,7,ii*mod(ii,7));
+    if exist('fig','var')
+        saveFigure(sessionConf,fig);
+        clear fig;
     end
+end
+
+function saveFigure(sessionConf,fig)
+    disp('Saving...');
+    saveas(fig,fullfile(sessionConf.nasPath,sessionConf.ratID,[sessionConf.ratID,'-graphs'],...
+                ['burstEventAnalysis_',datestr(now,'yyyymmdd-HHMMSS')]),'pdf');
+    close(fig);
 end
