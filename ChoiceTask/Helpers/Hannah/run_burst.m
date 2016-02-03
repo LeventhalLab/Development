69be9ff73e69ed06c94a4d06b29933d56fd83a8a
soiferh <soiferh@umich.edu>
2016-02-03 11:49:19 -0500

    Plotting Poisson bursts

--- ChoiceTask/Helpers/Hannah/run_burst.m
+++ ChoiceTask/Helpers/Hannah/run_burst.m
@@ -1,6 +1,6 @@
 % run burst, change paths
 [sev,header] = ezSEV();
-[n, ts] = nex_ts('/Users/mattgaidica/Documents/Data/ChoiceTask/R0088/R0088-processed/R0088_20151027a/R0088_20151027a_T5_ch[33 35 37 39].nex', 'Channel01c');
+[n, ts] = nex_ts('/Users/mattgaidica/Documents/Data/ChoiceTask/R0088/R0088-processed/R0088_20151027a/R0088_20151027a_T5_ch[33 35 37 39].nex', 'Channel01a');
 [archive_burst_RS,archive_burst_length,archive_burst_start]=burst(ts);
 
 %Get first 20 starting points
@@ -16,8 +16,9 @@ end
 [b, a] = butter(4, [.02, .5]);
 fdata = filtfilt(b,a,double(sev));
 figure;
-plot(fdata(1:1000000))
+plot(fdata(1:500000)/header.Fs)
 hold on;
-plot(ts(y(1:length(y)))*header.Fs,zeros(1,length(y)),'o');
+plot(ts(rand_start(1:20)),zeros(1,20),'*');
 hold on;
-plot(ts(rand_start(1:20))*header.Fs,zeros(1,20),'*');
+plot(ts(y),zeros(1,length(y)),'o');
+legend('start', 'end')
