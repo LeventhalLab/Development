fb59424a0ad2f11113b1b2379a86ed7ee3684ce6
soiferh <soiferh@umich.edu>
2016-02-08 11:05:19 -0500

    get sleep epochs

--- ChoiceTask/Helpers/Hannah/getFrameDiff.m
+++ ChoiceTask/Helpers/Hannah/getFrameDiff.m
@@ -1,15 +1,20 @@
-function [diffArray] = getFrameDiff(videoFileName)
+function [smoothDiffArray, sleepEpochs] = getFrameDiff(videoFileName)
 % Function to get the diffArray for a rat video
 % Input: 
 %   videoFileName - rat video file to analyze
 % Output: 
-%   diffArray - array of differences between pixels
+%   smoothDiffArray - array of differences between pixels
+%`  sleepEpochs - n x 2 array of frame indexes, start and end points of
+%                   periods of inactivity, determined by threshold
 
 v = VideoReader(videoFileName);
-%Take every 20th frame
 framesInterval = 20;
 diffArray = [];
 ii = 1;
+smoothFactor = .2;
+sleepEpochStart = [];
+sleepEpochEnd = [];
+thresh = 400;
  
 %Go through frames and find difference in pixels
 hWait = waitbar(0,'Analyzing Frames');
@@ -31,13 +36,20 @@ for f = 1 : framesInterval : v.NumberOfFrames - framesInterval
     ii = ii + 1;
 end
 close(hWait); clear('hWait');
-% t = linspace(0, length(diffArray) - 1, length(diffArray));
-% a = zeros(1, length(t));
-% a = a + 400;
-% smoothDiffArray = smooth(abs(diffArray), .2);
-% plot(t, smoothDiffArray)
-% hold on;
-% plot(t, a, '--')
-% axis([0 500 0 2500])
+
+smoothDiffArray = smooth(abs(diffArray), smoothFactor);
+
+for jj = 1:length(smoothDiffArray) - 1
+    if smoothDiffArray(jj) < thresh && smoothDiffArray(jj - 1) >= thresh
+        sleepEpochStart = [sleepEpochStart jj];
+    elseif smoothDiffArray(jj) < thresh && smoothDiffArray(jj + 1) > thresh
+        sleepEpochEnd = [sleepEpochEnd jj];
+    else
+    end
+end
+
+sleepEpochs = [sleepEpochStart, sleepEpochEnd];
+%to get time stamps, multiply by framesInterval and then divide by
+%frameRate
 
 end
--- ChoiceTask/Helpers/Hannah/sleepVideo.m
+++ ChoiceTask/Helpers/Hannah/sleepVideo.m
@@ -1,26 +1,27 @@
-function sleepVideo(v, diffArray, filename)
+function sleepVideo(vFileName, smoothDiffArray, filename)
 % Function to create a video file with rat and plot of activity
 % Input: 
 %   v - VideoReader object
 %   diffArray - output from getFrameDiff
 %   filename - the name of the video being created
-
+v = VideoReader(vFileName);
 framesInterval = 20;
+thresh = 400;
+smoothFactor = .2;
 
 %Create VideoWriter Object
 newVideo = VideoWriter(filename, 'Motion JPEG AVI');
 newVideo.Quality = 75;
 newVideo.FrameRate = v.FrameRate;
 open(newVideo);
-t = linspace(0, length(diffArray) - 1, length(diffArray));
-thresh = zeros(1, length(t)) + 400;
+t = linspace(0, length(smoothDiffArray) - 1, length(smoothDiffArray));
+threshArray = zeros(1, length(t)) + thresh;
 h = figure('Position',[0 0 600 600]);
 ii = 1;
-smoothDiffArray = smooth(abs(diffArray), .2);
 for f = 1 : framesInterval : v.NumberOfFrames - framesInterval
     %Read one frame
     curFrame = read(v, f);
-    curFrame = imresize(curFrame, 1/3);
+%    curFrame = imresize(curFrame, 1/3);
     hVid = subplot(2,1,1);
     set(hVid,'Units','pixels');
     imshow(curFrame,'border','tight');
@@ -30,7 +31,7 @@ for f = 1 : framesInterval : v.NumberOfFrames - framesInterval
     subplot(212);
     plot(t, smoothDiffArray)
     hold on;
-    plot(t, thresh, '--')
+    plot(t, threshArray, '--')
     axis([0 500 0 2500])
     xlabel('Time (s)');
     hold on;
