b100b9d059c4558b9c0830f78ace78859939b3d3
Matt Gaidica <matt@gaidi.ca>
2016-10-03 22:14:01 -0400

    Using scalograms

--- ChoiceTask/tsScaloXcorr.m
+++ ChoiceTask/tsScaloXcorr.m
@@ -3,7 +3,7 @@
 % [ ] handle slow firing units like 20160504a T24
 
 % sevFile = '/Users/mattgaidica/Documents/Data/ChoiceTask/R0117/R0117-rawdata/R0117_20160504a/R0117_20160504a/R0117_20160504a_R0117_20160504a-2_data_ch102.sev';
-[sev,header] = read_tdt_sev(sevFile);
+% [sev,header] = read_tdt_sev(sevFile);
 ts = nexStruct.neurons{2,1}.timestamps;
 
 decimateFactor = 10;
@@ -30,28 +30,34 @@ spansUpper = findThreshSpans(s,upperThresh,occurs);
 spansMiddle = findThreshSpans(s,[lowerThresh upperThresh],occurs);
 spansLower = findThreshSpans(s,-lowerThresh,occurs);
 
-window = 0.25; % s
-windowHalfSamples = round(Fs * (window / 2));
+window = 1; % s
+windowSamples = round(Fs * window);
 
-figure;
+fpass = [10 100];
+freqList = logFreqList(fpass,30);
+% figure;
 allSpans = {spansLower,spansMiddle,spansUpper};
 for iSpan = 1:3
     A = [];
+    scaloData = [];
     curSpans = allSpans{iSpan};
     for ii=1:size(curSpans,1)
         midSpan = mean(curSpans(ii,:)) / 1000; % seconds
-        sampleRange = [(round(midSpan * Fs) - windowHalfSamples):(round(midSpan * Fs) + windowHalfSamples)-1];
+        sampleRange = [(round(midSpan * Fs) - windowSamples):(round(midSpan * Fs) + windowSamples)-1];
         if min(sampleRange) > 0 && max(sampleRange) < length(sevFilt)
-            [At,f] = simpleFFT(sevFilt(sampleRange),Fs);
-            A(ii,:) = At;
+            scaloData(:,ii) = sevFilt(sampleRange);
+%             [At,f] = simpleFFT(sevFilt(sampleRange),Fs);
+%             A(ii,:) = At;
         end
     end
-
-    Am = mean(A);
-    hold on;
-    semilogy(f,smooth(Am,round(Fs/1000)));
+    [W, freqList] = calculateComplexScalograms_EnMasse(scaloData,'Fs',Fs,'fpass',fpass,'freqList',freqList,'doplot',true);
+    set(gca,'YScale','log');
+    set(gca,'Ytick',round(exp(linspace(log(min(freqList)),log(max(freqList)),5))));
+%     Am = mean(A);
+%     hold on;
+%     semilogy(f,smooth(Am,round(Fs/1000)));
 end
-legend('Lower','Middle','Upper');
-xlim([10 100]);
-xlabel('Frequency (Hz)');
-ylabel('|Y(f)|');
+% legend('Lower','Middle','Upper');
+% xlim([10 100]);
+% xlabel('Frequency (Hz)');
+% ylabel('|Y(f)|');
