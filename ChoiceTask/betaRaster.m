a8f7745ae6c6318fe5454baedd4b8d41cc92dceb
Matt Gaidica <matt@gaidi.ca>
2016-10-07 20:55:52 -0400

    The highest beta epochs are usually on centerOut, need to make function

--- ChoiceTask/betaRaster.m
+++ ChoiceTask/betaRaster.m
@@ -1,4 +1,4 @@
-ts = nexStruct.neurons{1,1}.timestamps;
+ts = nexStruct.neurons{4,1}.timestamps;
 if false
     tsBurst = [];
     tsLTS = [];
@@ -15,42 +15,72 @@ if false
     end
 end
 
-tts = tsBurst;
-% sevFile = '/Users/mattgaidica/Documents/Data/ChoiceTask/R0088/R0088-rawdata/R0088_20151102a/R0088_20151102a/R0088_20151102_R0088_20151102-1_data_ch35.sev';
+tts = ts;
+sevFile = '/Users/mattgaidica/Documents/Data/ChoiceTask/R0088/R0088-rawdata/R0088_20151102a/R0088_20151102a/R0088_20151102_R0088_20151102-1_data_ch34.sev';
 decimateFactor = 100;
-upperPrctile = 80;
+upperPrctile = 98;
 lowerPrctile = 15;
-spikeWindow = 0.5; %s
+spikeWindow = 5; %s
 
-% [sev,header] = read_tdt_sev(sevFile);
-% Fs = header.Fs/decimateFactor;
-% sevFilt = decimate(double(sev),decimateFactor);
-% sevFilt = eegfilt(sevFilt,Fs,13,30);
+[sev,header] = read_tdt_sev(sevFile);
+Fs = header.Fs/decimateFactor;
+sevFilt = decimate(double(sev),decimateFactor);
+sevFilt = eegfilt(sevFilt,Fs,13,30);
 
 x = hilbert(sevFilt);
 instAmp = abs(x); % envelope
 
 upperThresh = prctile(instAmp,upperPrctile);
-[locs,pks] = peakseek(instAmp,Fs/100,upperThresh);
+[locs,pks] = peakseek(instAmp,Fs,upperThresh);
 
 locs = locs(pks<300); % artifacts
+pks = pks(pks<300);
 
-%       M x 1 cell of spike times:
-%           M is the number of trials and each cell contains a 1 x N vector
-%           of spike times. Units should be in seconds.
-rasterTs = {}
+allLogEvent = [];
+correctTrials = find([trials.correct]==1);
+eventCount = 1;
+for iTrial=correctTrials
+    allLogEvent(eventCount) = trials(iTrial).timestamps.centerOut;
+    eventCount = eventCount + 1;
+end
+
+rasterTs = {};
+rasterEvents = {};
 allTs = [];
+allEvents = [];
 for ii=1:length(locs)
-    centerTs = round(locs(ii) / Fs);
+    centerTs = locs(ii) / Fs;
+    % should be only one unless spikeWindow >> trial length
+    rasterEvents{ii} = allLogEvent(allLogEvent < centerTs + spikeWindow & allLogEvent >= centerTs - spikeWindow) - centerTs;
     rasterTs{ii,1} = tts(tts < centerTs + spikeWindow & tts >= centerTs - spikeWindow)' - centerTs;
+    allEvents = [allEvents rasterEvents{ii}];
     allTs = [allTs rasterTs{ii,1}];
 end
 
+% stack high firing epochs on top 
+% [~,I] = sort(cellfun('size',rasterTs,2),'descend');
+% rasterTs = rasterTs(I);
+% rasterEvents = rasterEvents(I);
+% [t,idx] = sort([rasterEvents{1,:}]);
+[pksSort,I] = sort(pks);
+rasterTs = rasterTs(I);
+rasterEvents = rasterEvents(I);
+
 figure;
-plotSpikeRaster(rasterTs,'PlotType','vertline','AutoLabel',true);
+plotSpikeRaster(rasterTs,'PlotType','scatter','AutoLabel',true);
+title('Sorted by lfp power (high power = high trial #)');
+hold on;
+for ii=1:length(rasterEvents)
+    if ~isempty(rasterEvents{ii})
+        plot(rasterEvents{ii},ii,'o','Color','red');
+    end
+end
 
 figure;
 hist(allTs,25);
+
+figure;
+hist(allEvents,25);
 
 % figure;
 % plot(linspace(0,length(sevFilt)/Fs,length(sevFilt)),instAmp);
