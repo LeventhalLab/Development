e826017453c4ef58afd0ea5a40d7d03cba0a491f
Matt Gaidica <matt@gaidi.ca>
2016-11-30 12:23:18 -0500

    Working with export_fig to make unit summaries

--- ChoiceTask/Analysis/Formatting/savePDF.m
+++ ChoiceTask/Analysis/Formatting/savePDF.m
@@ -1,11 +1,14 @@
 function savePDF(h,leventhalPaths,subFolder,docName)
-set(h,'PaperOrientation','landscape');
-set(h,'PaperUnits','normalized');
-set(h,'PaperPosition', [0 0 1 1]);
+% set(h,'PaperOrientation','landscape');
+% set(h,'PaperUnits','normalized');
+% set(h,'PaperPosition', [.05 .05 .9 .9]);
 
 newDir = fullfile(leventhalPaths.analysis,subFolder);
 if ~exist(newDir,'dir')
     mkdir(newDir);
 end
-saveas(h,fullfile(leventhalPaths.analysis,subFolder,[docName,'.pdf']));
+% use transparency for overlaying later
+% pdftk f2.pdf background f1.pdf output text.pdf
+export_fig(h,fullfile(leventhalPaths.analysis,subFolder,docName),'-pdf','-transparent','-nocrop');
+% !/usr/local/bin/pdftk f1.pdf background bkgd.pdf output text.pdf
 close(h);
