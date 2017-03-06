5217ec8439e3df9ad671c1e8a072b01db4b106aa
Matt Gaidica <matt@gaidi.ca>
2017-03-06 12:40:28 -0500

    Wrapped up NNC analysis

--- ChoiceTask/On Hold/run_6ohdaSingleChs.m
+++ ChoiceTask/On Hold/run_6ohdaSingleChs.m
@@ -1,21 +1,37 @@
-r6ohdaFile = '/Users/mattgaidica/Documents/Data/ChoiceTask/R0153_20170214_openField-1/R0153_20170214_openField_R0153_20170214_openField-1_data_ch63.sev';
-rnormFile = '/Users/mattgaidica/Documents/Data/ChoiceTask/R0154_20170214_openField-2/R0154_20170214_openField_R0154_20170214_openField-2_data_ch63.sev';
+% chs = [1 3 5 7 34 36 38 40 41 43 45 47]; % MTHAL
+% target = 'MTHAL';
 
-params.fpass = [0 80];
-params.pad = 0;
-params.tapers = [3 5];
+chs = [58 60 62 64 19 21 23 25 18 20 22 24]; % STR
+target = 'STR';
 
-[sev,header] = read_tdt_sev(r6ohdaFile);
-params.Fs = round(header.Fs);
-[S,f] = mtspectrumc(sev',params);
-figure;
-plot_vector(smooth(S,300),f,'l',[],'b');
+figure('position',[500 500 700 300]);
 
+for iCh = 1:numel(chs)
+    disp(['ch',num2str(chs(iCh)),' ',target]);
+    rVEHFile = ['/Volumes/RecordingsLeventhal2/ChoiceTask/R0154/R0154-rawdata/R0154_20170227a/R0154_20170227a/R0154_20170227a_R0154_20170227a-1_data_ch',num2str(chs(iCh)),'.sev'];
+    rNNCFile = ['/Volumes/RecordingsLeventhal2/ChoiceTask/R0154/R0154-rawdata/R1054_20170228a/R0154_20170228a/R0154_20170228a_R0154_20170228a-1_data_ch',num2str(chs(iCh)),'.sev'];
 
+    params.fpass = [5 35];
+    params.pad = 0;
+    params.tapers = [3 5];
+    nSmooth = 2000;
+    nDown = 10;
 
-hold on;
+    [sevNNC,header] = read_tdt_sev(rNNCFile);
+    sevNNC = downsample(double(sevNNC),nDown);
+    params.Fs = round(header.Fs/nDown);
+    [SNNC,f] = mtspectrumc(sevNNC',params);
 
-[sev,header] = read_tdt_sev(rnormFile);
-params.Fs = round(header.Fs);
-[S,f] = mtspectrumc(sev',params);
-plot_vector(smooth(S,300),f,'l',[],'r');
+    [sevVEH,header] = read_tdt_sev(rVEHFile);
+    sevVEH = downsample(double(sevVEH),nDown);
+    params.Fs = round(header.Fs/nDown);
+    [SVEH,f] = mtspectrumc(sevVEH',params);
+
+    subplot(4,3,iCh);
+    plot_vector(smooth(SNNC,nSmooth),f,'l',[],'b');
+    hold on;
+    plot_vector(smooth(SVEH,nSmooth),f,'l',[],'r');
+
+    legend('NNC','Vehicle');
+    title(['ch',num2str(chs(iCh)),' ',target]);
+end
