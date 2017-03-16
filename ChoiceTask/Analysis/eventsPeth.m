926ed9387f01fdf864cc0c37c6e55bfc0b33db1e
Matt Gaidica <matt@gaidi.ca>
2017-03-16 13:35:06 -0400

    Changes from R0142 in D725

--- ChoiceTask/Analysis/eventsPeth.m
+++ ChoiceTask/Analysis/eventsPeth.m
@@ -5,6 +5,9 @@ for iTrial=1:numel(trials)
     eventFieldnames = fieldnames(trials(iTrial).timestamps);
     for iField=1:numel(eventFieldnames)
         centerTs = getfield(trials(iTrial).timestamps,eventFieldnames{iField});
+        if numel(centerTs) > 1 % handle weird case of two entries
+            centerTs = centerTs(1);
+        end
         peths{iTrial,iField} = tsPeth(ts,centerTs,tWindow);
     end
 end
--- ChoiceTask/run_code.m
+++ ChoiceTask/run_code.m
@@ -1,25 +1,5 @@
-testDir = '/Users/mattgaidica/Desktop/test-2';
-files = dir(fullfile(testDir,'*.sev'));
-filenames = natsort({files(:).name})';
-
-channels = [9 11 13 15];
-
-figure('position',[0 0 900 500]);
-for iCh = 1:4
-    subplot(4,1,iCh);
-    curCh = channels(iCh);
-    curFile = fullfile(testDir,filenames{curCh});
-    [fdata,header] = filterSev(curFile);
-    if iCh == 1 && ~exist('x','var')
-        h1 = figure;
-        plot(fdata);
-        [x,y] = ginput(1);
-        x = round(x);
-        close(h1);
-    end
-    ax(iCh) = plot(fdata(x:x+round(header.Fs)));
-    ylim([-500 500]);
-    drawnow;
+for ii=[5 6 7 8 9 10 11 12]
+    sessions__name = ['R0142_201612',num2str(ii,'%02d'),'a'];
+    sessionConf = exportSessionConfv2(sessions__name,'nasPath',nasPath);
+    nexData = TDTtoNex(sessionConf);
 end
-
-linkaxes(ax);
