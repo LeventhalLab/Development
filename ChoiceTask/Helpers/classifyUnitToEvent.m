1ee978d174a4017d0b84f6876337ca3f75a51a98
Matt Gaidica <matt@gaidi.ca>
2017-03-20 16:04:35 -0400

    Need to use session mean z scores, not individual

--- ChoiceTask/Helpers/classifyUnitToEvent.m
+++ ChoiceTask/Helpers/classifyUnitToEvent.m
@@ -1,17 +1,20 @@
-% % timingField = 'RT';
-% % [sorted_petz,sorted_times] = sortEventPetz(all_eventPetz,allTrials,timingField,[]);
-% % all_r = [];
-% % for iEvent = 1:size(sorted_petz,2)
-% %     eventPetz = sorted_petz{1,iEvent};
-% %     for iTrial = 1:size(eventPetz,1)
-% %         trialPetz = eventPetz(iTrial,:);
-% %         x = linspace(-tWindow,tWindow,size(trialPetz,2));
-% %         norm = normpdf(x,0,.25);
-% %         r = xcorr(norm,trialPetz);
-% %         all_r(iEvent,iTrial) = mean(r);
-% %     end
-% % end
-% % 
+timingField = 'RT';
+subjects__name = 'R0088';
+% eventFieldnames = fieldnames(trials(2).timestamps);
+tWindow = 2;
+[sorted_petz,sorted_times] = sortEventPetz(all_eventPetz,allTrials,timingField,[]);
+all_r = [];
+for iEvent = 1:size(sorted_petz,2)
+    iEventPetz = sorted_petz{1,iEvent};
+    for iTrial = 1:size(iEventPetz,1)
+        trialPetz = iEventPetz(iTrial,:);
+        x = linspace(-tWindow,tWindow,size(trialPetz,2));
+        norm = normpdf(x,0,.25);
+        r = xcorr(norm,trialPetz);
+        all_r(iEvent,iTrial) = mean(r);
+    end
+end
+
 [~,k] = max(all_r);
 % figure;
 % hist(k,size(all_r,1));
@@ -22,7 +25,7 @@ for ii=1:2
     subplot(1,3,ii);
     if ii==1
         imagesc(all_r');
-        title('sorted by RT');
+        title({subjects__name,'sorted by RT'});
         ylabel('trials');
     else
         imagesc(all_r(:,k2)');
@@ -32,7 +35,8 @@ for ii=1:2
     colorbar;
     caxis([-600 600]);
     xticks(1:8);
-    xlabel('event');
+    xticklabels(eventFieldnames);
+    xtickangle(90);
 end
 subplot(133);
 [counts,centers] = hist(k,8);
@@ -40,6 +44,7 @@ bar(centers,counts);
 xticks(centers);
 xticklabels([1:8]);
 xlim([1 8]);
-xlabel('event');
+xticklabels(eventFieldnames);
+xtickangle(90);
 title('event distribution');
 grid on;
