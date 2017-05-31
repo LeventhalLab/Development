8950a8683a2530118d4a777a5710ce2356a5a09d
Matt Gaidica <matt@gaidi.ca>
2017-05-30 22:10:34 -0400

    analyzing opto with RHD

--- Optogenetics/run_analyzeOptoNex_RHD.m
+++ Optogenetics/run_analyzeOptoNex_RHD.m
@@ -2,17 +2,18 @@ dosteps = [1,2]; % step 1: select laser epoch, step 2: generate plots
 protocol = 1; % 1 = long, 2 = short, 3 = custom
 useUnits = []; % empty runs plots for all units in NEX file
 
-if true % make false if data is already in MATLAB; save time, time = money, money is everything
+useChannel = 0;
+if false % make false if data is already in MATLAB; save time, time = money, money is everything
     % extract system-specific data
-    laserFile = '/Volumes/RecordingsLeventhal2/ChoiceTask/R0181/R0181-opto/R0181_20170525_cylinder/R0181_20170525c_cylinder-1/R0181_20170525c_cylinder_R0181_20170525c_cylinder-1_data_ch65.sev';
-    [laserData,header] = read_tdt_sev(laserFile);
-
-    ephysFile = '/Volumes/RecordingsLeventhal2/ChoiceTask/R0181/R0181-opto/R0181_20170525_cylinder/R0181_20170525c_cylinder-1/R0181_20170525c_cylinder_R0181_20170525c_cylinder-1_data_ch49.sev';
-    [ephysData,header] = read_tdt_sev(ephysFile);
+    rhdFile = '/Volumes/RecordingsLeventhal2/OptoEphys/R0177/R0177-rawdata/052417_opto/R0177_052417_optoephys1_170524_172440.rhd';
+    read_Intan_RHD2000_file(rhdFile);
 end
 
-% may contain multiple unit timestamps
-nexFile = '/Volumes/RecordingsLeventhal2/ChoiceTask/R0181/R0181-opto/R0181_20170525_cylinder/R0181_20170525c_cylinder-1/R0181_20170525c_cylinder_R0181_20170525c_cylinder-1_data_ch49.nex';
+ephysData = amplifier_data(useChannel+1,:);
+laserData = board_adc_data(1,:);
 
-Fs = header.Fs;
+% may contain multiple unit timestamps
+nexFile = '/Volumes/RecordingsLeventhal2/OptoEphys/R0177/R0177-rawdata/052417_opto/DDT Files/R0177_052417_optoephys1/R0177_052417_channel_0.nex';
+
+Fs = frequency_parameters.amplifier_sample_rate;
 analyzeOptoNex(ephysData,laserData,nexFile,Fs,dosteps,protocol,useUnits);
