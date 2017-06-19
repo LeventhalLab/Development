cfb83e71641506cb1bc1d333cabe5437da49745a
Ashish Kamath <adkamath@umich.edu>
2017-06-19 13:33:36 -0400

    Add files via upload

---
+++ ChoiceTask/Helpers/Ashish/run_UnitSummariesTANs.m
@@ -0,0 +1,8 @@
+startDir = '/Users/ashishkamath/Desktop/Research/TANs in STR Project/R0137';
+% change directory as needed
+textFiles = dir(fullfile(startDir,'*.txt'));
+
+for iFile = 1:size(textFiles,1)
+    fileName = fullfile(startDir,textFiles(iFile).name);
+    makeUnitSummaries_updated(fileName);
+end
