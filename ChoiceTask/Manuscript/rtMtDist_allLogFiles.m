027477d2ad922d6d7eb99e8453f587a6b84d37ed
Matt Gaidica <matt@gaidi.ca>
2017-08-16 15:30:01 -0400

    Not sure about this surrogate thing

--- ChoiceTask/Manuscript/rtMtDist_allLogFiles.m
+++ ChoiceTask/Manuscript/rtMtDist_allLogFiles.m
@@ -3,7 +3,7 @@ logPaths = {'/Volumes/RecordingsLeventhal2/ChoiceTask/R0088',...
     '/Volumes/RecordingsLeventhal2/ChoiceTask/R0142',...
     '/Volumes/RecordingsLeventhal2/ChoiceTask/R0154'};
 
-if true
+if false
     all_RT = [];
     all_MT = [];
     allLog_RT = {};
@@ -18,12 +18,17 @@ if true
                 logData = readLogData(logFile);
                 if isfield(logData,'outcome')
                     corrIdx = find(logData.outcome == 0);
-                    if (numel(corrIdx) / numel(logData.outcome)) > 0.7
-                        allLog_RT{iSession} = logData.RT(corrIdx);
-                        allLog_MT{iSession} = logData.MT(corrIdx);
-                        all_RT = [all_RT logData.RT(corrIdx)];
-                        all_MT = [all_MT logData.MT(corrIdx)];
-                        iSession = iSession + 1;
+                    if (numel(corrIdx) / numel(logData.outcome)) > 0.5
+                        RTs = logData.RT(corrIdx);
+                        MTs = logData.MT(corrIdx);
+                        validIdxs = find(RTs > 0 & RTs < 1 & MTs > 0 & MTs < 1);
+                        if ~isempty(validIdxs)
+                            allLog_RT{iSession} = RTs(validIdxs);
+                            allLog_MT{iSession} = MTs(validIdxs);
+                            all_RT = [all_RT;RTs(validIdxs)];
+                            all_MT = [all_MT;MTs(validIdxs)];
+                            iSession = iSession + 1;
+                        end
                     end
                 end
             end
@@ -31,34 +36,54 @@ if true
     end
 end
 
-
-
-colors = lines(numel(allLog_RT));
-figure;
-for iSession = 1:numel(allLog_RT)
-    plot(allLog_RT{iSession},allLog_MT{iSession},'.','MarkerSize',10,'color',colors(iSession,:));
-    hold on;
+if true
+    colors = lines(numel(allLog_RT));
+    figure;
+    for iSession = 1:numel(allLog_RT)
+        plot(allLog_RT{iSession},allLog_MT{iSession},'.','MarkerSize',10,'color',colors(iSession,:));
+        hold on;
+    end
+    xlabel('RT');
+    ylabel('MT');
+    xlim([0 1]);
+    ylim([0 1]);
 end
+
+% if you fix MT
+tickLabels = {};
+blockInterval = 0.1;
+fRTs = 0:blockInterval:1;
+fMTs = 0:blockInterval:1;
+nSurr = 100;
+probMatrix = [];
+figure;
+for iRT = 1:numel(fRTs)
+    fRT = fRTs(iRT);
+    tickLabels{iRT} = [num2str(fRT),' - ',num2str(fRT + blockInterval)];
+    RTIdx = find(all_RT >= fRT & all_RT < fRT + blockInterval);
+    MTsinRTbin = all_MT(RTIdx);
+    for iMT = 1:numel(fMTs)
+        fMT = fMTs(iMT);
+        rtMtIdx = find(MTsinRTbin >= fMT & MTsinRTbin < fMT + blockInterval);
+        surrArr = [];
+        for iSurr = 1:nSurr
+%             randIdx = randperm(numel(MTsinRTbin));
+%             all_MT_shuff = MTsinRTbin(randIdx);
+%             rtMtShuffIdx = find(all_MT_shuff >= fMT & all_MT_shuff < fMT + blockInterval);
+%             surrArr(iSurr) = numel(rtMtShuffIdx);
+            randsample(MTsinRTbin,numel(rtMtIdx));
+        end
+        
+        probMatrix(iMT,iRT) = numel(find(numel(rtMtIdx) > surrArr)) / numel(surrArr);
+        imagesc(probMatrix); drawnow; set(gca,'ydir','normal');
+    end
+end
+set(gca,'ydir','normal');
 xlabel('RT');
 ylabel('MT');
-xlim([0 1]);
-ylim([0 1]);
-
-figure;
-for iSession = 1:numel(allLog_RT)
-    plot(allLog_RT{iSession},allLog_RT{iSession}+allLog_MT{iSession},'k.','MarkerSize',10);
-    hold on;
-end
-
-% % figure;
-% % for iSession = 1:numel(allLog_RT)
-% %     plot(allLog_RT{iSession}+allLog_MT{iSession},'k.','MarkerSize',10);
-% %     hold on;
-% % end
-% % for iSession = 1:numel(allLog_RT)
-% %     rndMT = allLog_MT{iSession};
-% %     rndIdx = randperm(numel(allLog_MT{iSession}));
-% %     rndMT = rndMT(rndIdx);
-% %     plot(allLog_RT{iSession}+rndMT,'r.','MarkerSize',10);
-% %     hold on;
-% % end
+xticklabels(tickLabels);
+xtickangle(90);
+yticklabels(tickLabels);
+colorbar;
+caxis([0 1]);
+colormap(jet);
