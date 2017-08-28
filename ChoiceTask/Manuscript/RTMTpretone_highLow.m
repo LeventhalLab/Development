c764d5e59062e41718d32a8bc6515b68694fc125
Matt Gaidica <matt@gaidi.ca>
2017-08-28 11:16:11 -0400

    One anatomical analysis, moving into directionally selection RT correlation

--- ChoiceTask/Manuscript/RTMTpretone_highLow.m
+++ ChoiceTask/Manuscript/RTMTpretone_highLow.m
@@ -1,9 +1,7 @@
-if true
+if false
     RTmin = 0;
     RTmax = median(all_rt) + std(all_rt);
-    RTmin = .15;
-    RTmax = .1811;
-    tWindow = 2;
+    tWindow = 1;
     [unitEventsRTlow,all_zscoresRTlow] = classifyUnitsToEvents(analysisConf,all_trials,all_ts,eventFieldnames,tWindow,binMs,trialTypes,useEvents,RTmin,RTmax);
     n_all_zscoresRTlow = numel(find(all_rt > RTmin & all_rt < RTmax));
 
@@ -13,19 +11,77 @@ if true
     n_all_zscoresRThigh = numel(find(all_rt > RTmin & all_rt < RTmax));
 end
 
-figuree(1200,400);
-colors = lines(2);
-for iEvent = 1:numel(eventFieldnames)
-    subplot(1,7,iEvent);
-    lns(1) = plot(smooth(nanmean(squeeze(all_zscoresRTlow(:,iEvent,:))),3),'LineWidth',1,'Color',colors(1,:));
-    hold on;
-    lns(2) = plot(smooth(nanmean(squeeze(all_zscoresRThigh(:,iEvent,:))),3),'LineWidth',1,'Color',colors(2,:));
-    xlim([1 80]);
-    xticks([1 40 80]);
-    xticklabels({num2str(-tWindow),'0',num2str(tWindow)});
-    ylim([-2 8]);
-    grid on;
-    title([eventFieldnames{iEvent}]);
+if false
+    figuree(1200,400);
+    colors = lines(2);
+    for iEvent = 1:numel(eventFieldnames)
+        subplot(1,7,iEvent);
+        lns(1) = plot(smooth(nanmean(squeeze(all_zscoresRTlow(:,iEvent,:))),3),'LineWidth',1,'Color',colors(1,:));
+        hold on;
+        lns(2) = plot(smooth(nanmean(squeeze(all_zscoresRThigh(:,iEvent,:))),3),'LineWidth',1,'Color',colors(2,:));
+        xlim([1 80]);
+        xticks([1 40 80]);
+        xticklabels({num2str(-tWindow),'0',num2str(tWindow)});
+        ylim([-2 8]);
+        grid on;
+        title([eventFieldnames{iEvent}]);
+    end
+
+    legend(lns,{['low RT ',num2str(n_all_zscoresRTlow)],['high RT ',num2str(n_all_zscoresRThigh)]});
 end
 
-legend(lns,{['low RT ',num2str(n_all_zscoresRTlow)],['high RT ',num2str(n_all_zscoresRThigh)]});
+if true
+    colors = lines(2);
+    y = [];
+    yCount = 1;
+    group = {};
+    figuree(1300,400);
+    for iNeuron = 1:size(all_zscoresRTlow,1)
+        lowMax = max(all_zscoresRTlow(iNeuron,4,:));
+        highMax = max(all_zscoresRThigh(iNeuron,4,:));
+        lowStd = std(all_zscoresRTlow(iNeuron,4,:));
+        highStd = std(all_zscoresRThigh(iNeuron,4,:));
+        if lowMax > 40 || highMax > 40 || lowStd == 0 || highStd == 0
+            continue;
+        end
+        for iEvent = 1:7
+            subplot(2,7,iEvent);
+            plot(smooth(squeeze(all_zscoresRTlow(iNeuron,iEvent,:)),3),'-','color',[colors(1,:) 0.15]);
+            ylim([-15 30]);
+            title({'RT Low',eventFieldnames{iEvent}});
+            xticks([1 round(size(all_zscores,3))/2 size(all_zscores,3)]);
+            xticklabels({'-1','0','1'});
+            hold on;
+            grid on;
+            
+            subplot(2,7,iEvent+7);
+            plot(smooth(squeeze(all_zscoresRThigh(iNeuron,iEvent,:)),3),'-','color',[colors(2,:) 0.15]);
+            ylim([-15 30]);
+            title({'RT High',eventFieldnames{iEvent}});
+            xticks([1 round(size(all_zscores,3))/2 size(all_zscores,3)]);
+            xticklabels({'-1','0','1'});
+            hold on;
+            grid on;
+            if iEvent == 1
+                ylabel('z');
+            end
+        end
+        
+        y(yCount) = lowStd;
+        group{yCount} = 'Low RT';
+        yCount = yCount + 1;
+        
+        y(yCount) = highStd;
+        group{yCount} = 'High RT';
+        yCount = yCount + 1;
+    end
+    for iEvent = 1:7
+        subplot(2,7,iEvent);
+        plot(smooth(squeeze(mean(all_zscoresRTlow(:,iEvent,:))),3),'-','color',colors(1,:),'lineWidth',2);
+
+        subplot(2,7,iEvent+7);
+        plot(smooth(squeeze(mean(all_zscoresRThigh(:,iEvent,:))),3),'-','color',colors(2,:),'lineWidth',2);
+    end
+    numel(y)
+    p = anova1(y,group);
+end
--- ChoiceTask/Manuscript/ipsiContraShuffle.m
+++ ChoiceTask/Manuscript/ipsiContraShuffle.m
@@ -1,57 +1,88 @@
 % the fraction of units whose activity is significantly different between
 % ipsi/contra trials
 
-useEvents = 1:7;
-trialTypes = {'correctContra','correctIpsi'};
+if false
+    useEvents = 1:7;
+    trialTypes = {'correctContra','correctIpsi'};
 
-requireTrials = 5;
-nShuffle = 1000;
-pNeuronDiff = [];
-pNeuronAll = [];
-for iNeuron = 1:numel(analysisConf.neurons)
-    neuronName = analysisConf.neurons{iNeuron};
-    curTrials = all_trials{iNeuron};
-    
-    trialIdInfo = organizeTrialsById(curTrials);
-    
-    if numel(trialIdInfo.correctContra) < requireTrials || numel(trialIdInfo.correctIpsi) < requireTrials
-        continue;
+    requireTrials = 5;
+    nShuffle = 1000;
+    pNeuronDiff = [];
+    pNeuronAll = [];
+    for iNeuron = 1:numel(analysisConf.neurons)
+        neuronName = analysisConf.neurons{iNeuron};
+        curTrials = all_trials{iNeuron};
+
+        trialIdInfo = organizeTrialsById(curTrials);
+
+        if numel(trialIdInfo.correctContra) < requireTrials || numel(trialIdInfo.correctIpsi) < requireTrials
+            continue;
+        end
+
+        useTrials = [trialIdInfo.correctContra trialIdInfo.correctIpsi];
+        trialClass = zeros(numel(useTrials),1);
+        trialClass(1:numel(trialIdInfo.correctContra)) = ones(numel(trialIdInfo.correctContra),1);
+
+
+        % (ordered according to useTrials)
+        tsPeths = eventsPeth(curTrials(useTrials),all_ts{iNeuron},tWindow,eventFieldnames);
+
+        pEventDiff = [];
+        pEventAll = [];
+        for iEvent = 1:numel(useEvents)
+            curPeths = tsPeths(:,iEvent);
+            eventMatrix = [];
+            for iTrial = 1:numel(curPeths)
+                [counts,centers] = hist(curPeths{iTrial},nBins_tWindow);
+                eventMatrix(iTrial,:) = counts;
+            end
+
+            contraMean = mean(eventMatrix(trialClass == 1,:));
+            ipsiMean = mean(eventMatrix(trialClass == 0,:));
+            matrixDiff = abs(contraMean - ipsiMean);
+            matrixDiffShuffle = [];
+            for iShuffle = 1:nShuffle
+                shuffledTrialTypes = trialClass(randperm(numel(trialClass)));
+                contraMeanShuffled = mean(eventMatrix(shuffledTrialTypes == 1,:));
+                ipsiMeanShuffled = mean(eventMatrix(shuffledTrialTypes == 0,:));
+                matrixDiffShuffle(iShuffle,:) = abs(contraMeanShuffled - ipsiMeanShuffled);
+            end
+            % how often is matrixDiff greater than matrixDiffShuffle?
+            for iBin = 1:numel(matrixDiff)
+                curMDS = matrixDiffShuffle(:,iBin);
+                pEventDiff(iEvent,iBin) = numel(find(matrixDiff(iBin) > curMDS)) / nShuffle;
+            end
+        end
+        pNeuronDiff(iNeuron,:,:) = pEventDiff;
+        pNeuronAll(iNeuron,:,:) = pEventAll;
     end
-    
-    useTrials = [trialIdInfo.correctContra trialIdInfo.correctIpsi];
-    trialClass = zeros(numel(useTrials),1);
-    trialClass(1:numel(trialIdInfo.correctContra)) = ones(numel(trialIdInfo.correctContra),1);
+end
+% see ipsiContraShuffle.m
+useEvents = [1:7];
 
-    
-    % (ordered according to useTrials)
-    tsPeths = eventsPeth(curTrials(useTrials),all_ts{iNeuron},tWindow,eventFieldnames);
-    
-    pEventDiff = [];
-    pEventAll = [];
-    for iEvent = 1:numel(useEvents)
-        curPeths = tsPeths(:,iEvent);
-        eventMatrix = [];
-        for iTrial = 1:numel(curPeths)
-            [counts,centers] = hist(curPeths{iTrial},nBins_tWindow);
-            eventMatrix(iTrial,:) = counts;
-        end
-
-        contraMean = mean(eventMatrix(trialClass == 1,:));
-        ipsiMean = mean(eventMatrix(trialClass == 0,:));
-        matrixDiff = abs(contraMean - ipsiMean);
-        matrixDiffShuffle = [];
-        for iShuffle = 1:nShuffle
-            shuffledTrialTypes = trialClass(randperm(numel(trialClass)));
-            contraMeanShuffled = mean(eventMatrix(shuffledTrialTypes == 1,:));
-            ipsiMeanShuffled = mean(eventMatrix(shuffledTrialTypes == 0,:));
-            matrixDiffShuffle(iShuffle,:) = abs(contraMeanShuffled - ipsiMeanShuffled);
-        end
-        % how often is matrixDiff greater than matrixDiffShuffle?
-        for iBin = 1:numel(matrixDiff)
-            curMDS = matrixDiffShuffle(:,iBin);
-            pEventDiff(iEvent,iBin) = numel(find(matrixDiff(iBin) > curMDS)) / nShuffle;
+pVal = 0.95;
+figuree(1200,400);
+for iEvent = 1:numel(useEvents)
+    subplot(1,numel(useEvents),iEvent)
+    eventBins = zeros(1,size(pNeuronDiff,3));
+    for iNeuron = 1:size(pNeuronDiff,1)
+        if ~isempty(unitEvents{iNeuron}.class)%% && unitEvents{iNeuron}.class(1) == 3 % tone
+            curBins = squeeze(pNeuronDiff(iNeuron,iEvent,:)); % 40 bins per-event
+            eventBins = eventBins + (curBins > pVal);
         end
     end
-    pNeuronDiff(iNeuron,:,:) = pEventDiff;
-    pNeuronAll(iNeuron,:,:) = pEventAll;
+
+%     bar(1:size(pNeuronDiff,3),eventBins/numel(toneNeurons),'FaceColor','k','EdgeColor','none'); % POSITIVE
+    bar(1:size(pNeuronDiff,3),eventBins/size(pNeuronDiff,1),'FaceColor','k','EdgeColor','none'); % POSITIVE
+    hold on;
+% %     ylim([0 0.4]);
+% %     yticks([0:0.2:0.4]);
+    xlim([1 40]);
+    xticks([1 20 40]);
+    xticklabels({'-1','0','1'});
+    grid on;
+    xlabel(eventFieldnames{iEvent});
+    if iEvent == 1
+        ylabel('Fraction of units < 0.05');
+    end
 end
