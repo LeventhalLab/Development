6269706ace54987ab6eafb252a6ccedaaa724221
Matt Gaidica <matt@gaidi.ca>
2017-11-20 15:10:14 -0500

    Tried a few methods of the selectivity index

--- ChoiceTask/Manuscript/selectivityIndex.m
+++ ChoiceTask/Manuscript/selectivityIndex.m
@@ -1,7 +1,7 @@
 % directionally selectivity index
 % Zleft-Zright / Zleft+Zright
 % scatter for all units: Nose Out Y, Side Out X
-analysisTitle = 'Tone Units';
+analysisTitle = 'All Units';
 
 localSideOutPath = '/Users/mattgaidica/Documents/Data/ChoiceTask/sideOutAnalysis';
 excludeSessions = {'R0117_20160504a','R0142_20161207a','R0117_20160508a','R0117_20160510a'}; % corrupt video
@@ -40,7 +40,7 @@ for iNeuron = 1:numel(analysisConf.neurons)
     if sum(M == 1) < minTrials || sum(M == 2) < minTrials
         continue;
     end
-%     if ~ismember(primSec(iNeuron,1),3)
+%     if ~ismember(primSec(iNeuron,1),4)
 %         continue;
 %     end
     
@@ -50,9 +50,14 @@ for iNeuron = 1:numel(analysisConf.neurons)
     
     curTs = all_ts{iNeuron};
     curTrials = all_trials{iNeuron};
-% %     z = zParams(curTs,curTrials);
-% %     zBinMean = z.FRmean * (binMs/1000);
-% %     zBinStd = z.FRstd * (binMs/1000);
+    z = zParams(curTs,curTrials);
+    zBinMean = z.FRmean * (binMs/1000);
+    zBinStd = z.FRstd * (binMs/1000);
+    % skip if no counts, can't determine mean/std
+    if zBinStd == 0 || zBinMean == 0
+        continue;
+    end
+    
     trialIdInfo = organizeTrialsById(curTrials);
     
     % tsPeths for all trial types
@@ -60,48 +65,50 @@ for iNeuron = 1:numel(analysisConf.neurons)
     
     % --- Nose Out
     useTrials = trialIdInfo.correctContra;
-    peths = [tsPeths{useTrials,4}];
-    hCounts = histcounts(peths,nBins_tWindow);
+    hCounts = histcounts([tsPeths{useTrials,4}],nBins_tWindow);
     if any(hCounts <= requireSpikes)
         neuronCount = neuronCount - 1;
         continue;
     end
-    contra_score = smooth(hCounts / numel(useTrials),nSmooth);
+    contra_zscore = ((hCounts / numel(useTrials)) - zBinMean) / zBinStd;
+    contra_score = smooth(hCounts ./ numel(useTrials),nSmooth);
     
     useTrials = trialIdInfo.correctIpsi;
-    peths = [tsPeths{useTrials,4}];
-    hCounts = histcounts(peths,nBins_tWindow);
+    hCounts = histcounts([tsPeths{useTrials,4}],nBins_tWindow);
     if any(hCounts <= requireSpikes)
         neuronCount = neuronCount - 1;
         continue;
     end
-    ipsi_score = smooth(hCounts / numel(useTrials),nSmooth);
-%     si_noseOut(neuronCount,:) = ((contra_score - ipsi_score) ./ (contra_score + ipsi_score));
-    si_noseOut(neuronCount,1) = max((contra_score(analyzeRange) - ipsi_score(analyzeRange)) ./ (contra_score(analyzeRange) + ipsi_score(analyzeRange)));
+    ipsi_zscore = ((hCounts / numel(useTrials)) - zBinMean) / zBinStd;
+    ipsi_score = smooth(hCounts ./ numel(useTrials),nSmooth);
+    
+%     si_noseOut(neuronCount,:) = max((contra_zscore - ipsi_zscore) ./ (contra_zscore + ipsi_zscore));
+    si_noseOut(neuronCount,:) = max((contra_score - ipsi_score) ./ (contra_score + ipsi_score));
     
     useTrials = find(M == 1);
-    peths = [tsPeths{useTrials,6}];
-    hCounts = histcounts(peths,nBins_tWindow);
+    hCounts = histcounts([tsPeths{useTrials,6}],nBins_tWindow);
     if any(hCounts <= requireSpikes)
         neuronCount = neuronCount - 1;
         continue;
     end
-    contra_score = smooth(hCounts / numel(useTrials),nSmooth);
+    contra_zscore = ((hCounts / numel(useTrials)) - zBinMean) / zBinStd;
+    contra_score = smooth(hCounts ./ numel(useTrials),nSmooth);
     
     useTrials = find(M == 2);
-    peths = [tsPeths{useTrials,6}];
-    hCounts = histcounts(peths,nBins_tWindow);
+    hCounts = histcounts([tsPeths{useTrials,6}],nBins_tWindow);
     if any(hCounts <= requireSpikes)
         neuronCount = neuronCount - 1;
         continue;
     end
-    ipsi_score = smooth(hCounts / numel(useTrials),nSmooth);
-%     si_sideOut(neuronCount,:) = ((contra_score - ipsi_score) ./ (contra_score + ipsi_score));
-    si_sideOut(neuronCount,1) = max((contra_score(analyzeRange) - ipsi_score(analyzeRange)) ./ (contra_score(analyzeRange) + ipsi_score(analyzeRange)));
+    ipsi_zscore = ((hCounts / numel(useTrials)) - zBinMean) / zBinStd;
+    ipsi_score = smooth(hCounts ./ numel(useTrials),nSmooth);
     
-    scatter_colors(neuronCount,:) = [0 0 0];
+%     si_sideOut(neuronCount,:) = max((contra_zscore - ipsi_zscore) ./ (contra_zscore + ipsi_zscore));
+    si_sideOut(neuronCount,:) = max((contra_score - ipsi_score) ./ (contra_score + ipsi_score));
+    
+    scatter_colors(neuronCount,:) = [1 0 0];
     if dirSelNeurons(iNeuron)
-        scatter_colors(neuronCount,:) = [1 0 0];
+        scatter_colors(neuronCount,:) = [0 0 0];
     end
     
     % skip if nan
@@ -116,35 +123,38 @@ for iNeuron = 1:numel(analysisConf.neurons)
 % %         scatter_colors(neuronCount,:) = [0 0 0];
 % %     end
 end
+% handle end case
+si_noseOut = si_noseOut(1:neuronCount,:);
+si_sideOut = si_sideOut(1:neuronCount,:);
 
-si_categories = zeros(size(si_sideOut));
-si_categories(si_noseOut > 0 & si_sideOut > 0) = 1;
-si_categories(si_noseOut > 0 & si_sideOut < 0) = 2;
-si_categories(si_noseOut < 0 & si_sideOut > 0) = 3;
-si_categories(si_noseOut < 0 & si_sideOut < 0) = 4;
-[h,pVal] = chi2gof(si_categories,'NBins',4)
+% % si_categories = zeros(size(si_sideOut));
+% % si_categories(si_noseOut > 0 & si_sideOut > 0) = 1;
+% % si_categories(si_noseOut > 0 & si_sideOut < 0) = 2;
+% % si_categories(si_noseOut < 0 & si_sideOut > 0) = 3;
+% % si_categories(si_noseOut < 0 & si_sideOut < 0) = 4;
+% % [h,pVal] = chi2gof(si_categories,'NBins',4)
 
 figuree(300,600);
 subplot(211);
 scatter(si_noseOut,si_sideOut,30,scatter_colors,'filled');
 xlabel('si Nose Out');
 ylabel('si Side Out');
-xlim([-1 1]);
-ylim([-1 1]);
-xticks([-1 0 1]);
-yticks([-1 0 1]);
+xlim([0 1]);
+ylim([0 1]);
+xticks(xlim);
+yticks(ylim);
 grid on;
 title(analysisTitle)
 setFig;
 
-subplot(212);
-labels = {'NO & SO > 0','NO > 0 & SO < 0','NO < 0 & SO > 0','NO & SO < 0'};
-si_quadrants = histcounts(si_categories,4);
-explode = [1 1 1 1];
-p = pie(si_quadrants,explode,labels);
-p(1).FaceColor = [1 0 0];
-p(3).FaceColor = repmat(0.5,1,3);
-p(5).FaceColor = p(3).FaceColor;
-p(7).FaceColor = p(3).FaceColor;
-title(['p = ',num2str(pVal)]);
-setFig;
+% % subplot(212);
+% % labels = {'NO & SO > 0','NO > 0 & SO < 0','NO < 0 & SO > 0','NO & SO < 0'};
+% % si_quadrants = histcounts(si_categories,4);
+% % explode = [1 1 1 1];
+% % p = pie(si_quadrants,explode,labels);
+% % p(1).FaceColor = [1 0 0];
+% % p(3).FaceColor = repmat(0.5,1,3);
+% % p(5).FaceColor = p(3).FaceColor;
+% % p(7).FaceColor = p(3).FaceColor;
+% % title(['p = ',num2str(pVal)]);
+% % setFig;
