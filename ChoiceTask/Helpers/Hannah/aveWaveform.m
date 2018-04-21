ff6693741eede47767e0e2e87c5d0286e389529e
Matt Gaidica <matt@gaidi.ca>
2018-04-21 15:56:30 -0400

    Almost done compiling waveforms for wires

--- ChoiceTask/Helpers/Hannah/aveWaveform.m
+++ ChoiceTask/Helpers/Hannah/aveWaveform.m
@@ -1,4 +1,4 @@
-function [meanWaveform, upperStd, lowerStd, ch, windowSize] = aveWaveform(ts, SEVfilename, varargin)
+function [meanWaveform, upperStd, lowerStd, windowSize] = aveWaveform(ts,sevFile,varargin)
 %Function to plot the average waveform of a unit with the peak centered at
 %zero. Default color of summer tree green. Default window size of 4 ms.
 %
@@ -13,25 +13,19 @@ function [meanWaveform, upperStd, lowerStd, ch, windowSize] = aveWaveform(ts, SE
 %   meanWaveforms - vector to plot the waveform later
 %   upperStd - the waveform one standard deviation above the mean
 %   lowerStd - the waveform one standard deviation below the mean
-%   ch - channel number
 %   windowSize 
 
-
-
-% color = [145/255, 205/255, 114/255];
 windowSize = .002;
 
 for iarg = 1: 2 : nargin - 2
     switch varargin{iarg}
-%         case 'color'
-%             color = varargin{iarg + 1};
         case 'windowSize'
             windowSize = varargin{iarg + 1}/1000;
     end
 end
 
 %Read in data and filter
-[sev, header] = read_tdt_sev(SEVfilename);
+[sev, header] = read_tdt_sev(sevFile);
 window = round((windowSize * header.Fs)/2);
 [b,a] = butter(4, [0.02 0.5]);
 sev = filtfilt(b,a,double(sev));
@@ -54,6 +48,5 @@ meanWaveform = mean(waveforms,1);
 stdDev = std(waveforms);
 upperStd = meanWaveform + stdDev;
 lowerStd = meanWaveform - stdDev;
-ch = getSEVChFromFilename(SEVfilename);
 
 end
---
+++ ChoiceTask/Manuscript/extractWaveforms.m
@@ -0,0 +1,56 @@
+function [waveforms,sameWire,wireLabels] = extractWaveforms(analysisConf,all_ts)
+    % same wire array: [1st wire start; 2nd wire start; ...]
+    % All first wire: x = sameWire{2}; -> idxs = z(1):z(2)-1;
+    % 366 rows x #
+    
+    waveforms = [];
+    sameWire = [];
+    
+    subjects__name = '';
+    wireLabels = {};
+    startNeuron = 1;
+    for iNeuron = 1:numel(analysisConf.neurons)
+        neuronName = analysisConf.neurons{iNeuron};
+        disp(['Working on ',neuronName]);
+
+        % unique wires
+        [electrodeName,electrodeSite,electrodeChannels] = getElectrodeInfo(neuronName);
+        sessionConf = analysisConf.sessionConfs{iNeuron};
+        if strcmp(subjects__name,sessionConf.subjects__name) == 0 || iNeuron == numel(analysisConf.neurons)
+            if ~isempty(subjects__name)
+                [C,ia,ic] = unique(subjectElectrodes);
+                wireLabels = {wireLabels{:} C{:}};
+                prototype = zeros(numel(analysisConf.neurons),numel(unique(ic)));
+                endNeuron = iNeuron - 1;
+                for iElectrode = 1:numel(unique(ic))
+                    prototype(startNeuron:endNeuron,iElectrode) = ic == iElectrode;
+                end
+                if isempty(sameWire)
+                    sameWire = prototype;
+                else
+                    sameWire = [sameWire prototype];
+                end
+                startNeuron = iNeuron;
+            end
+            subjects__name = sessionConf.subjects__name;
+            subjectElectrodes = {};
+            electrodeCount = 0;
+        end
+        electrodeCount = electrodeCount + 1;
+        subjectElectrodes{electrodeCount} = electrodeName;
+
+        % waveforms
+        ts = all_ts{iNeuron};
+        meanWaveforms = [];
+        for iChannel = 1:numel(electrodeChannels)
+            sevFile = sessionConf.sevFiles{electrodeChannels(iChannel)};
+            [meanWaveform, upperStd, lowerStd, ch, windowSize] = aveWaveform(ts,sevFile);
+            meanWaveforms(iChannel,:) = meanWaveform;
+        end
+        if size(meanWaveforms,1) > 1
+            waveforms(iNeuron,:) = mean(meanWaveforms);
+        else
+            waveforms(iNeuron,:) = meanWaveforms;
+        end
+    end
+end
