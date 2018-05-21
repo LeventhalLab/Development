e67fb28df1ee47579ae8b040978c3f50bede26d5
Matt Gaidica <matt@gaidi.ca>
2018-05-21 09:50:21 -0400

    Building all_Ws

--- ChoiceTask/LFPs/utils/build_all_Ws.m
+++ ChoiceTask/LFPs/utils/build_all_Ws.m
@@ -5,14 +5,14 @@ freqList = logFreqList(fpass,30);
 tWindow = 1;
 
 all_Ws = {};
-for iFile = 1:numel(LFPfiles)
-    disp(LFPfiles{iFile});
-    sevFile = LFPfiles{iFile};
+for iNeuron = 1:numel(LFPfiles)
+    disp(LFPfiles{iNeuron});
+    sevFile = LFPfiles{iNeuron};
     [sev,header] = read_tdt_sev(sevFile);
     sevFilt = decimate(double(sev),decimateFactor);
     Fs = header.Fs / decimateFactor;
     
-    curTrials = all_trials{iFile};
+    curTrials = all_trials{iNeuron};
     trialIds = sortTrialsBy(curTrials,'RT');
     allW = eventsLFP(curTrials(trialIds),sevFilt,tWindow,Fs,freqList,eventFieldnames);
     all_Ws{iNeuron} = allW;
