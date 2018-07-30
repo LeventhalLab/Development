32159fd84647dd7615fefa8de5cd84f30c72f625
Matt Gaidica <matt@gaidi.ca>
2018-07-30 11:09:42 -0400

    reloading session, compiling averages for each plot

--- ChoiceTask/LFPs/explore/spikeLFPxcorrBySession.m
+++ ChoiceTask/LFPs/explore/spikeLFPxcorrBySession.m
@@ -1,6 +1,6 @@
 doSetup = true;
-doDebug = true;
-nSurr = 10;
+doDebug = false;
+nSurr = 100;
 zThresh = 2;
 freqList = logFreqList([1 200],30);
 ytickIds = [1 closest(freqList,20) closest(freqList,55) numel(freqList)]; % selected from freqList
@@ -13,33 +13,33 @@ Wlength = 200;
 tWindow = 1;
 t = linspace(-tWindow,tWindow,Wlength);
 
-for iSession = 15%:numel(sessionNames)
+all_xcorrBands_events = {};
+all_surr_result = {};
+for iSession = 1:numel(sessionNames)
     if doSetup
         sessionTs = [];
-        for iNeuron = find(ia == iSession)'
+        sessionUnits = find(ia == iSession)';
+        for iNeuron = sessionUnits % sessionUnits(primSec(sessionUnits,1)==4)
             sessionTs = [sessionTs;all_ts{iNeuron}];
         end
         sessionTs = sort(sessionTs);
         
-%         sevFile = LFPfiles_local{selectedLFPFiles(iSession)};
-%         curTrials = all_trials{iNeuron};
-%         [trialIds,allTimes] = sortTrialsBy(curTrials,'RT');
-%         [sevFilt,Fs,decimateFactor] = loadCompressedSEV(sevFile);
-%         W = eventsLFPv2(curTrials(trialIds),sevFilt,tWindow,Fs,freqList,eventFieldnames);
-%         [Wz_power,Wz_phase] = zScoreW(W,Wlength); % power Z-score
-%         [Wz_power,keepTrials] = removeWzTrials(Wz_power,zThresh);
-%         
-%         tsPeths = eventsPeth(curTrials(trialIds(keepTrials)),sessionTs,2,eventFieldnames); % tWindow=2
+        sevFile = LFPfiles_local{selectedLFPFiles(iSession)};
+        curTrials = all_trials{iNeuron};
+        [trialIds,allTimes] = sortTrialsBy(curTrials,'RT');
+        [sevFilt,Fs,decimateFactor] = loadCompressedSEV(sevFile);
+        W = eventsLFPv2(curTrials(trialIds),sevFilt,tWindow,Fs,freqList,eventFieldnames);
+        [Wz_power,Wz_phase] = zScoreW(W,Wlength); % power Z-score
+        [Wz_power,keepTrials] = removeWzTrials(Wz_power,zThresh);
+        
+        tsPeths = eventsPeth(curTrials(trialIds(keepTrials)),sessionTs,2,eventFieldnames); % tWindow=2
+        [refMean,refStd] = getRefsForZscore(tsPeths,tWindow,Wlength);
         
         all_SDEz = [];
         xcorrBands_events = [];
         for iEvent = 1:size(tsPeths,2)
             for iTrial = 1:size(tsPeths,1)
                 thisSDE = get_SDE(tsPeths{iTrial,iEvent},tWindow,Wlength);
-                if iEvent == 1
-                    refMean = mean(thisSDE);
-                    refStd = std(thisSDE);
-                end
                 thisSDEz = (thisSDE - refMean) ./ refStd;
                 all_SDEz(iEvent,iTrial,:) = thisSDEz;
                 xcorrBands = [];
@@ -50,14 +50,6 @@ for iSession = 15%:numel(sessionNames)
                     xcorrBands(:,iBand) = r;
                 end
                 xcorrBands_trial(iTrial,:,:) = xcorrBands;
-                if iEvent == 4 && iTrial == 1
-                    figure;
-                    imagesc(xcorrBands');
-                    colormap(jet);
-                    set(gca,'ydir','normal');
-                    xlim([100 300])
-                    grid on;
-                end
             end
             xcorrBands_events(iEvent,:,:) = squeeze(mean(xcorrBands_trial));
         end
@@ -68,10 +60,6 @@ for iSession = 15%:numel(sessionNames)
             for iEvent = 1:size(tsPeths,2)
                 for iTrial = 1:size(tsPeths,1)
                     thisSDE = get_SDE(tsPeths{iTrial_perm(iTrial),iEvent},tWindow,Wlength);
-                    if iEvent == 1
-                        refMean = mean(thisSDE);
-                        refStd = std(thisSDE);
-                    end
                     thisSDEz = (thisSDE - refMean) ./ refStd;
                     xcorrBands = [];
                     for iBand = 1:numel(freqList)
@@ -96,19 +84,17 @@ for iSession = 15%:numel(sessionNames)
         end
     end
     
+    t_xcorr = linspace(-tWindow*2,tWindow*2,size(xcorrBands,1));
+    
     % -- DEBUG
     if doDebug
-        savePath = '/Users/mattgaidica/Documents/Data/ChoiceTask/LFPs/perievent/xcorrBySession';
+        savePath = '/Users/mattgaidica/Documents/Data/ChoiceTask/LFPs/perievent/xcorrBySession/debug';
         rows = 3;
         cols = 7;
         for iTrial = 1:size(tsPeths,1)
             h = figuree(1300,500);
             for iEvent = 1:size(tsPeths,2)
                 thisSDE = get_SDE(tsPeths{iTrial,iEvent},tWindow,Wlength);
-                if iEvent == 1
-                    refMean = mean(thisSDE);
-                    refStd = std(thisSDE);
-                end
                 thisSDEz = (thisSDE - refMean) ./ refStd;
                 all_SDEz(iEvent,iTrial,:) = thisSDEz;
                 xcorrBands = [];
@@ -117,15 +103,6 @@ for iSession = 15%:numel(sessionNames)
                     y = thisSDEz;
                     [r,lags] = xcorr(x,y);
                     xcorrBands(:,iBand) = r;
-                end
-                
-                if iEvent == 4 && iTrial == 1
-                    figure;
-                    imagesc(xcorrBands');
-                    colormap(jet);
-                    set(gca,'ydir','normal');
-                    xlim([100 300])
-                    grid on;
                 end
 
                 subplot(rows,cols,prc(cols,[1,iEvent]));
@@ -141,8 +118,8 @@ for iSession = 15%:numel(sessionNames)
                     cb = cbAside(gca,'trial power','k');
                     cb.Ticks = caxis;
                 end
-                primCount = sum(primSec(find(ia == iSession),1) == iEvent);
-                secCount = sum(primSec(find(ia == iSession),2) == iEvent);
+                primCount = sum(primSec(sessionUnits,1) == iEvent);
+                secCount = sum(primSec(sessionUnits,2) == iEvent);
                 if iEvent == 1
                     title({['t',num2str(iTrial),', u',num2str(find(ia == iSession,1)),'-',num2str(find(ia == iSession,1,'last'))],...
                         [eventFieldnames{iEvent}]});
@@ -182,17 +159,17 @@ for iSession = 15%:numel(sessionNames)
                 grid on;
             end
             set(gcf,'color','w');
-            saveFile = ['debug_t',num2str(iTrial),'_u',num2str(find(ia == iSession,1)),'-',num2str(find(ia == iSession,1,'last'))];
+            saveFile = ['debug_u',num2str(find(ia == iSession,1),'%03d'),'-',num2str(find(ia == iSession,1,'last'),'%03d'),...
+                '_t',num2str(iTrial,'%03d')];
             saveas(h,fullfile(savePath,[saveFile,'.png']));
             close(h);
         end
     end
     % -- DEBUG
 
-    figuree(1300,900);
+    h = figuree(1300,900);
     rows = 4;
     cols = 7;
-    t_xcorr = linspace(-tWindow*2,tWindow*2,size(xcorrBands,1));
     for iEvent = 1:7
         subplot(rows,cols,prc(cols,[1,iEvent]));
         imagesc(t,1:numel(freqList),squeeze(mean(squeeze(Wz_power(iEvent,:,:,:)),2))');
@@ -207,8 +184,8 @@ for iSession = 15%:numel(sessionNames)
             cb = cbAside(gca,'mean power','k');
             cb.Ticks = caxis;
         end
-        primCount = sum(primSec(find(ia == iSession),1) == iEvent);
-        secCount = sum(primSec(find(ia == iSession),2) == iEvent);
+        primCount = sum(primSec(sessionUnits,1) == iEvent);
+        secCount = sum(primSec(sessionUnits,2) == iEvent);
         if iEvent == 1
             title({['s',num2str(iSession),', u',num2str(find(ia == iSession,1)),'-',num2str(find(ia == iSession,1,'last'))],...
                 [eventFieldnames{iEvent}],[num2str(primCount),'^{primary}, ',num2str(secCount),'^{secondary}']});
@@ -241,7 +218,7 @@ for iSession = 15%:numel(sessionNames)
         if iEvent == 1
             ylabel('freq (Hz)');
         end
-        caxis(round(minmaxRed(xcorrBands_events)));
+        caxis(round(minmaxRed(xcorrBands_events(3,:,:))));
         if iEvent == 7
             cb = cbAside(gca,'mean xcorr','k');
             cb.Ticks = caxis;
@@ -269,25 +246,43 @@ for iSession = 15%:numel(sessionNames)
         caxis([-1 1]);
         grid on;
     end
+    all_xcorrBands_events{iSession} = xcorrBands_events;
+    all_surr_result{iSession} = surr_result;
+    
     set(gcf,'color','w');
+    savePath = '/Users/mattgaidica/Documents/Data/ChoiceTask/LFPs/perievent/xcorrBySession';
+    saveFile = ['debug_u',num2str(find(ia == iSession,1),'%03d'),'-',num2str(find(ia == iSession,1,'last'),'%03d'),...
+        '_s',num2str(iSession,'%02d')];
+    saveas(h,fullfile(savePath,[saveFile,'.png']));
+    close(h);
 end
 
 
+function [refMean,refStd] = getRefsForZscore(tsPeths,tWindow,Wlength)
+    for iTrial = 1:size(tsPeths,1)
+        thisSDE = get_SDE(tsPeths{iTrial,1},tWindow,Wlength);
+        all_mean(iTrial) = mean(thisSDE);
+        all_std(iTrial) = std(thisSDE);
+    end
+    refMean = mean(all_mean);
+    refStd = mean(all_std);
+end
+
 % attempting to keep settings self-contained
 function s = get_SDE(ts,tWindow,nBins)
-sigma = .020; % kernel std
-sigmaMult = 3;
-% binWidth = .001; % 1ms
-% binEdges = -tWindow:binWidth:tWindow;
-binEdges = linspace(-tWindow-sigmaMult*sigma,tWindow+sigmaMult*sigma,nBins+1+(2*sigmaMult));
-binWidth = mean(diff(binEdges));
-counts = histcounts(ts,binEdges); % bin data
-edges = -sigmaMult*sigma:binWidth:sigmaMult*sigma; % time ranges
-kernel = normpdf(edges,0,sigma); % eval guassian kernel
-kernel = kernel*binWidth; % multiply by bin width
-sConv = conv(counts,kernel); % convolve
+    sigma = .020; % kernel std
+    sigmaMult = 3;
+    % binWidth = .001; % 1ms
+    % binEdges = -tWindow:binWidth:tWindow;
+    binEdges = linspace(-tWindow-sigmaMult*sigma,tWindow+sigmaMult*sigma,nBins+1+(2*sigmaMult));
+    binWidth = mean(diff(binEdges));
+    counts = histcounts(ts,binEdges); % bin data
+    edges = -sigmaMult*sigma:binWidth:sigmaMult*sigma; % time ranges
+    kernel = normpdf(edges,0,sigma); % eval guassian kernel
+    kernel = kernel*binWidth; % multiply by bin width
+    sConv = conv(counts,kernel); % convolve
 
-halfKernel = ceil(numel(edges)/2); % index of kernel center
-s = sConv(halfKernel:halfKernel + numel(counts) - 1); % remove kernel smoothing from edges
-s = s(sigmaMult+1:end-sigmaMult);
+    halfKernel = ceil(numel(edges)/2); % index of kernel center
+    s = sConv(halfKernel:halfKernel + numel(counts) - 1); % remove kernel smoothing from edges
+    s = s(sigmaMult+1:end-sigmaMult);
 end
