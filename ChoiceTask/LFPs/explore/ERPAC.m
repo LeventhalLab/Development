fef1f1ef6377b863b42f11c245b77b74e95e1908
Matt Gaidica <matt@gaidi.ca>
2018-08-03 19:19:14 -0400

    First rev of PAC (event-related)

---
+++ ChoiceTask/LFPs/explore/ERPAC.m
@@ -0,0 +1,100 @@
+doSetup = true;
+doSave = true;
+savePath = '/Users/mattgaidica/Documents/Data/ChoiceTask/LFPs/ERPAC';
+zThresh = 2;
+tWindow = 1;
+freqList = logFreqList([3.5 200],30);
+
+freqIdx = floor(linspace(1,numel(freqList),5));
+freqLabels = freqList(freqIdx);
+freqLabels = num2str(freqLabels(:),'%2.1f');
+views = [0 0;45 15; 80 15];
+pvalThresh = 0.01;
+
+iSession = 0;
+all_M = [];
+for iNeuron = selectedLFPFiles'
+    iSession = iSession + 1;
+    if doSetup
+        sevFile = LFPfiles_local{iNeuron};
+        disp(sevFile);
+        [~,name,~] = fileparts(sevFile);
+        subjectName = name(1:5);
+        curTrials = all_trials{iNeuron};
+        [trialIds,allTimes] = sortTrialsBy(curTrials,'RT');
+        [sevFilt,Fs,decimateFactor] = loadCompressedSEV(sevFile);
+        W = eventsLFPv2(curTrials(trialIds),sevFilt,tWindow,Fs,freqList,eventFieldnames);
+        [Wz_power,Wz_phase] = zScoreW(W,Wlength); % power Z-score
+        [Wz_power,keepTrials] = removeWzTrials(Wz_power,zThresh);
+        Wz_phase = Wz_phase(:,:,keepTrials,:);
+    
+    
+        ERPAC_rho = NaN(size(Wz_power,1),size(Wz_power,2),size(Wz_power,4),size(Wz_power,4));
+        ERPAC_pval = NaN(size(Wz_power,1),size(Wz_power,2),size(Wz_power,4),size(Wz_power,4));
+        for iEvent = 1:size(Wz_power,1)
+            for iBin = 1:size(Wz_power,2)
+                for iFreq = 1:numel(freqList)
+                    for jFreq = iFreq:numel(freqList)
+                        alpha = squeeze(Wz_phase(iEvent,iBin,:,iFreq));
+                        x = squeeze(Wz_phase(iEvent,iBin,:,jFreq));
+                        [rho,pval] = circ_corrcl(alpha,x);
+                        ERPAC_rho(iEvent,iBin,iFreq,jFreq) = rho;
+                        ERPAC_pval(iEvent,iBin,iFreq,jFreq) = pval;
+                    end
+                end
+            end
+        end
+    end
+    
+    t = linspace(-1,1,size(ERPAC_rho,2));
+    M = [];
+    for iBin = 1:size(ERPAC_rho,2)
+        for iEvent = 1:7
+            ERPAC_matrix = squeeze(ERPAC_rho(iEvent,iBin,:,:));
+            ERPAC_matrix_pval = squeeze(ERPAC_pval(iEvent,iBin,:,:)) < pvalThresh;
+            im = (ERPAC_matrix_pval.*ERPAC_matrix)';
+            im(im == 0) = NaN;
+            M(iEvent,iBin,:,:) = im;
+        end
+    end
+    all_M(iSession,:,:,:,:) = M;
+    h = figuree(1300,900);
+    rows = 3;
+    cols = 7;
+    for iRow = 1:rows
+        for iEvent = 1:cols
+            subplot(rows,cols,prc(cols,[iRow,iEvent]));
+            hm = slice(squeeze(M(iEvent,:,:,:)),[],1:size(ERPAC_rho,2),[]);
+% %             [xs,ys,zs] = ndgrid( 1:25 , 1:50 , 1:4 ) ;
+            shading interp;
+            colormap(jet);
+            caxis([0 1]);
+            set(hm,'FaceAlpha',0.3);
+            view(views(iRow,:));
+            xlabel('amplitude (Hz)');
+            zlabel('phase (Hz)');
+            ylabel('time (s)');
+            xticks(freqIdx);
+            xticklabels(freqLabels);
+            zticks(freqIdx);
+            zticklabels(freqLabels);
+            yticks([1 round(size(ERPAC_rho,2)/2) size(ERPAC_rho,2)]);
+            yticklabels([-tWindow,0,tWindow]);
+            set(gca,'fontsize',7);
+            if iRow == 1
+                title(eventFieldnames{iEvent});
+            end
+%             if iEvent == 7
+%                 cb = colorbar;
+%                 cb.Ticks = caxis;
+%             end
+        end
+    end
+    set(gcf,'color','w');
+    if doSave
+        saveFile = [subjectName,'_s',num2str(iSession,'%02d'),'_ERPAC'];
+        saveas(h,fullfile(savePath,[saveFile,'.fig']));
+        saveas(h,fullfile(savePath,[saveFile,'.png']));
+        close(h);
+    end
+end
--- ChoiceTask/LFPs/explore/aggregateDKLTransients.m
+++ ChoiceTask/LFPs/explore/aggregateDKLTransients.m
@@ -38,10 +38,102 @@ if doSetup
     end
 end
 
+figuree(1200,500);
+rows = 2;
+cols = 4;
+nBins = 10;
+for iTiming = 1:2
+    if iTiming == 1
+        iEvent = 4;
+    else
+        iEvent = 6;
+    end
+    x_ff = [];
+    y_ff = [];
+    x_cv = [];
+    y_cv = [];
+    for iSession = 1:size(compiledTimes,1)
+        allTimes = compiledTimes{iSession,iTiming};
+        locs_dkl = compiledDKL{iSession,iTiming,iEvent};
+%         locs_dkl = compiledJones{iSession,iTiming,iEvent};
+        for iTrial = 1:numel(locs_dkl)
+            if numel(locs_dkl{iTrial}) > 1
+                x_cv = [x_cv allTimes(iTrial)];
+                y_cv = [y_cv mean(diff(locs_dkl{iTrial}))];
+            end
+            x_ff = [x_ff allTimes(iTrial)];
+            y_ff = [y_ff numel(locs_dkl{iTrial})];
+        end
+    end
+    [x_cv_sorted,x_cv_key] = sort(x_cv);
+    y_cv_sorted = y_cv(x_cv_key);
+    binIdxs = floor(linspace(1,numel(x_cv),nBins+1));
+    data_mean = [];
+    data_std = [];
+    data_var = [];
+    for iBin = 1:nBins
+        data_mean(iBin) = mean(y_cv_sorted(binIdxs(iBin):binIdxs(iBin+1)));
+        data_std(iBin) = std(y_cv_sorted(binIdxs(iBin):binIdxs(iBin+1)));
+        data_var(iBin) = var(y_cv_sorted(binIdxs(iBin):binIdxs(iBin+1)));
+    end
+    subplot(rows,cols,prc(cols,[iTiming,1]));
+    errorbar(1:nBins,data_mean,data_std,'k');
+    xlim([0 nBins+1]);
+    xticks(0:nBins+1);
+    xticklabels({'',1:nBins,''});
+    title('IEI (samples) +/- std');
+    xlabel([timingFields{iTiming},' bracket']);
+    ylim([0 1200]);
+    ylabel(eventFieldnames{iEvent});
+    grid on;
+    
+    subplot(rows,cols,prc(cols,[iTiming,2]));
+    plot(data_var./(data_mean.^2),'k');
+    xlim([0 nBins+1]);
+    xticks(0:nBins+1);
+    xticklabels({'',1:nBins,''});
+    title('CV^2 (var/mean^2)');
+    ylim([0.25 0.5]);
+    xlabel([timingFields{iTiming},' bracket']);
+    grid on;
+    
+    [x_ff_sorted,x_ff_key] = sort(x_ff);
+    y_ff_sorted = y_ff(x_ff_key);
+    binIdxs = floor(linspace(1,numel(x_ff),nBins+1));
+    data_mean = [];
+    data_std = [];
+    data_var = [];
+    for iBin = 1:nBins
+        data_mean(iBin) = mean(y_ff_sorted(binIdxs(iBin):binIdxs(iBin+1)));
+        data_std(iBin) = std(y_ff_sorted(binIdxs(iBin):binIdxs(iBin+1)));
+        data_var(iBin) = var(y_ff_sorted(binIdxs(iBin):binIdxs(iBin+1)));
+    end
+    subplot(rows,cols,prc(cols,[iTiming,3]));
+    errorbar(1:nBins,data_mean,data_std,'k');
+    xlim([0 nBins+1]);
+    xticks(0:nBins+1);
+    xticklabels({'',1:nBins,''});
+    ylim([1 6]);
+    title('# events +/- std');
+    xlabel([timingFields{iTiming},' bracket']);
+    grid on;
+    
+    subplot(rows,cols,prc(cols,[iTiming,4]));
+    plot(data_var./data_mean,'k');
+    xlim([0 nBins+1]);
+    xticks(0:nBins+1);
+    xticklabels({'',1:nBins,''});
+    title('FF (var/mean)');
+    ylim([0.75 1.25]);
+    xlabel([timingFields{iTiming},' bracket']);
+    grid on;
+end
+set(gcf,'color','w');
+
 figuree(1400,900);
 rows = 4;
 cols = 7;
-nLines = 10;
+nLines = 3;
 nBins = 30;
 colors = cool(nLines);
 for iTiming = 1:2
@@ -118,7 +210,7 @@ for iTiming = 1:2
         end
         xlim([-1 1]);
         xticks(sort([xlim 0]));
-        ylim([-5 20]);
+        ylim([-5 25]);
         ylabel('z');
         grid on;
     end
