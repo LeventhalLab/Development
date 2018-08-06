50ff7d3711ef54c1c1ead58f76d135c00de39675
Matt Gaidica <matt@gaidi.ca>
2018-08-06 12:41:34 -0400

    Creating video spin for ERPAC

--- ChoiceTask/LFPs/explore/ERPAC.m
+++ ChoiceTask/LFPs/explore/ERPAC.m
@@ -36,7 +36,7 @@ for iNeuron = selectedLFPFiles'
                 for iFreq = 1:numel(freqList)
                     for jFreq = iFreq:numel(freqList)
                         alpha = squeeze(Wz_phase(iEvent,iBin,:,iFreq));
-                        x = squeeze(Wz_phase(iEvent,iBin,:,jFreq));
+                        x = squeeze(Wz_power(iEvent,iBin,:,jFreq));
                         [rho,pval] = circ_corrcl(alpha,x);
                         ERPAC_rho(iEvent,iBin,iFreq,jFreq) = rho;
                         ERPAC_pval(iEvent,iBin,iFreq,jFreq) = pval;
@@ -52,49 +52,100 @@ for iNeuron = selectedLFPFiles'
         for iEvent = 1:7
             ERPAC_matrix = squeeze(ERPAC_rho(iEvent,iBin,:,:));
             ERPAC_matrix_pval = squeeze(ERPAC_pval(iEvent,iBin,:,:)) < pvalThresh;
-            im = (ERPAC_matrix_pval.*ERPAC_matrix)';
+%             im = (ERPAC_matrix_pval.*ERPAC_matrix)';
+            im = ERPAC_matrix;
             im(im == 0) = NaN;
             M(iEvent,iBin,:,:) = im;
         end
     end
     all_M(iSession,:,:,:,:) = M;
-    h = figuree(1300,900);
-    rows = 3;
-    cols = 7;
-    for iRow = 1:rows
-        for iEvent = 1:cols
-            subplot(rows,cols,prc(cols,[iRow,iEvent]));
-            hm = slice(squeeze(M(iEvent,:,:,:)),[],1:size(ERPAC_rho,2),[]);
-% %             [xs,ys,zs] = ndgrid( 1:25 , 1:50 , 1:4 ) ;
-            shading interp;
-            colormap(jet);
-            caxis([0 1]);
-            set(hm,'FaceAlpha',0.3);
-            view(views(iRow,:));
-            xlabel('amplitude (Hz)');
-            zlabel('phase (Hz)');
-            ylabel('time (s)');
-            xticks(freqIdx);
-            xticklabels(freqLabels);
-            zticks(freqIdx);
-            zticklabels(freqLabels);
-            yticks([1 round(size(ERPAC_rho,2)/2) size(ERPAC_rho,2)]);
-            yticklabels([-tWindow,0,tWindow]);
-            set(gca,'fontsize',7);
-            if iRow == 1
-                title(eventFieldnames{iEvent});
-            end
-%             if iEvent == 7
-%                 cb = colorbar;
-%                 cb.Ticks = caxis;
-%             end
-        end
-    end
-    set(gcf,'color','w');
     if doSave
+        h = figuree(1500,550);
+        rows = 3;
+        cols = 7;
+        for iRow = 1:rows
+            for iEvent = 1:cols
+                subplot(rows,cols,prc(cols,[iRow,iEvent]));
+                hm = slice(squeeze(M(iEvent,:,:,:)),[],1:size(ERPAC_rho,2),[]);
+    % %             [xs,ys,zs] = ndgrid( 1:25 , 1:50 , 1:4 ) ;
+                shading interp;
+                colormap(jet);
+                caxis([0 0.5]);
+                set(hm,'FaceAlpha',0.1);
+                view(views(iRow,:));
+                xlabel('amp (Hz)');
+                zlabel('phase (Hz)');
+                ylabel('time (s)');
+                xticks(freqIdx);
+                xticklabels(freqLabels);
+                zticks(freqIdx);
+                zticklabels(freqLabels);
+                yticks([1 round(size(ERPAC_rho,2)/2) size(ERPAC_rho,2)]);
+                yticklabels([-tWindow,0,tWindow]);
+                set(gca,'fontsize',7);
+                if iRow == 1
+                    title(eventFieldnames{iEvent});
+                end
+                if iEvent == 7
+                    cb = cbAside(gca,['corr, p <',num2str(pvalThresh,'%1.2f')],'k');
+                end
+            end
+        end
+        set(gcf,'color','w');
+
         saveFile = [subjectName,'_s',num2str(iSession,'%02d'),'_ERPAC'];
         saveas(h,fullfile(savePath,[saveFile,'.fig']));
         saveas(h,fullfile(savePath,[saveFile,'.png']));
         close(h);
     end
+end
+
+h = figuree(1500,700);
+rows = 4;
+cols = 7;
+mean_M = squeeze(nanmean(all_M));
+for iEvent = 1:cols
+    for iRow = 1:3
+        subplot(rows,cols,prc(cols,[iRow,iEvent]));
+        hm = slice(squeeze(mean_M(iEvent,:,:,:)),[],1:size(ERPAC_rho,2),[]);
+% %             [xs,ys,zs] = ndgrid( 1:25 , 1:50 , 1:4 ) ;
+        shading interp;
+        colormap(jet);
+        caxis([0.25 0.5]);
+        set(hm,'FaceAlpha',0.3);
+        view(views(iRow,:));
+        xlabel('amp (Hz)');
+        zlabel('phase (Hz)');
+        ylabel('time (s)');
+        xticks(freqIdx);
+        xticklabels(freqLabels);
+        zticks(freqIdx);
+        zticklabels(freqLabels);
+        yticks([1 round(size(ERPAC_rho,2)/2) size(ERPAC_rho,2)]);
+        yticklabels([-tWindow,0,tWindow]);
+        set(gca,'fontsize',7);
+        if iRow == 1
+            title(eventFieldnames{iEvent});
+        end
+        if iEvent == 7
+            cb = cbAside(gca,['corr, p <',num2str(pvalThresh,'%1.2f')],'k');
+        end
+    end
+    subplot(rows,cols,prc(cols,[4,iEvent]));
+    plot(t,squeeze(nansum(squeeze(nansum(squeeze(mean_M(iEvent,:,:,:)),2)),2)),'k-','lineWidth',2);
+    xlim([-1 1]);
+    xticks(sort([xlim,0]));
+    ylim([0 200]);
+%     yticks(sort([ylim,0.5]));
+    if iEvent == 1
+        ylabel('nansum, all freq');
+    end
+    grid on;
+end
+set(gcf,'color','w');
+if doSave
+    saveFile = ['allSessions_ERPAC'];
+    saveas(h,fullfile(savePath,[saveFile,'.fig']));
+    saveas(h,fullfile(savePath,[saveFile,'.png']));
+    close(h);
 end
