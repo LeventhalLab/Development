ed728fe1c9c659635780394202dd19a4cd985a9e
Matt Gaidica <matt@gaidi.ca>
2018-09-17 19:46:58 -0400

    delta RT/MT working for all events

--- ChoiceTask/LFPs/explore/deltaRTcorr.m
+++ ChoiceTask/LFPs/explore/deltaRTcorr.m
@@ -1,14 +1,14 @@
 doSetup = true;
-doTiming = 'RT';
+doTiming = 'MT';
 
-doPlot1 = true;
-doPlot2 = false;
+% % doPlot1 = false; % !! commented, update to cell structure (with events) to use
+doPlot2 = true;
 doSave = false;
 savePath = '/Users/mattgaidica/Documents/Data/ChoiceTask/LFPs/wholeSession/spikePhaseDeltaRT';
 
 tWindow = 1;
-% % freqList = [1.8 20]; % wavelet method
-freqList = {[1 3;13 30]}; % hilbert method
+freqList = [1.8]; % wavelet method
+% freqList = {[1 3]}; % hilbert method
 if strcmp(doTiming,'RT')
     iEvent = 3;
     caxisVals = [0.1 0.3];
@@ -19,152 +19,146 @@ end
 
 if doSetup
     n_timePoints = 101;
-    phaseCorr = [];
-    phaseCorrs_delta = [];
-    phaseCorrs_beta = [];
-    betaCorrs = [];
     all_Times = [];
+    phaseCorrs_delta = {};
     for iNeuron = selectedLFPFiles'
         iSession = iSession + 1;
         disp(num2str(iNeuron));
         sevFile = LFPfiles_local{iNeuron};
         [~,name,~] = fileparts(sevFile);
-        [sevFilt,Fs,decimateFactor] = loadCompressedSEV(sevFile);
+        [sevFilt,Fs,decimateFactor] = loadCompressedSEV(sevFile,[]);
         curTrials = all_trials{iNeuron};
 
         [trialIds,allTimes] = sortTrialsBy(curTrials,doTiming);
         W = eventsLFPv2(curTrials(trialIds),sevFilt,tWindow,Fs,freqList,eventFieldnames);
         
         tIdxs = floor(linspace(1,size(W,2),n_timePoints));
-        phaseCorrs_delta = [phaseCorrs_delta;squeeze(angle(W(iEvent,tIdxs,:,1)))'];
-        betaZ = (squeeze(abs(W(iEvent,tIdxs,:,2)).^2) - mean(mean(squeeze(abs(W(1,:,:,2)).^2)))) ./ mean(std(squeeze(abs(W(1,:,:,2)).^2)));
-        phaseCorrs_beta = [phaseCorrs_beta;betaZ'];
-% %         betaCorrs = [betaCorrs;squeeze(abs(W(4,tIdxs,:,2)).^2)'];      
-% %         t0 = round(size(W,2)/2);
-% %         phaseCorr = [phaseCorr;squeeze(angle(W(iEvent,t0,:)))];
+        for iEvent = 1:7
+            if iNeuron == 1
+                phaseCorrs_delta{iEvent} = [];
+            end
+            phaseCorrs_delta{iEvent} = [phaseCorrs_delta{iEvent};squeeze(angle(W(iEvent,tIdxs,:)))'];
+        end
         all_Times = [all_Times;allTimes'];
     end
-end
-
-if false
-    figure;
-    imagesc(angle(squeeze(W(3,:,:,1)))');
-    colormap(cmap_phase);
 end
 
 cmapPath = '/Users/mattgaidica/Documents/MATLAB/LeventhalLab/Development/ChoiceTask/LFPs/utils/corr_colormap.jpg';
 cmap = mycmap(cmapPath,n_timePoints);
 rows = ceil(sqrt(n_timePoints));
-rows = 5;
 cols = rows;
 nBins = 24;
 
-
 binEdges = linspace(-pi,pi,nBins+1);
 binCenters = linspace(-pi,pi,nBins);
-binEdges = linspace(-1,5,nBins+1);
-binCenters = linspace(-1,5,nBins);
 
 timeCenters = linspace(-1,1,n_timePoints);
 iSubplot = 1;
-if doPlot1
-    h = figuree(900,800);
-end
-all_meanTimes = [];
-all_pvals = [];
-all_rhos = [];
+% % if doPlot1
+% %     h = figuree(900,800);
+% %     timeLabels = {};
+% %     useRange = 31:55;
+% %     all_pvals = [];
+% %     all_rhos = [];
+% % end
 
-% useRange = 31:55;
-useRange = round(linspace(30,90,25));
-
-timeLabels = {};
-for ii = useRange%n_timePoints % !! CHANGE BACK
-    phaseCorr = phaseCorrs_beta(:,ii);
-    meanTimes = [];
-    stdTimes = [];
-    
-    for iBin = 1:nBins
-        theseTimes = all_Times(phaseCorr >= binEdges(iBin) & phaseCorr < binEdges(iBin+1));
-        meanTimes(iBin) = mean(theseTimes);
-        stdTimes(iBin) = std(theseTimes);
-    end
-    [rho,pval] = circ_corrcl(phaseCorr,all_Times);
-    all_pvals(iSubplot) = pval;
-    all_rhos(iSubplot) = rho;
-    
-    all_meanTimes(ii,:) = meanTimes;
-    if doPlot1
-        subplot(rows,cols,iSubplot);
-        % STD
+all_meanTimes = {};
+for iEvent = 1:7
+    event_meanTimes = [];
+    for ii = 1:n_timePoints
+        phaseCorr = phaseCorrs_delta{iEvent}(:,ii);
+        meanTimes = [];
+        stdTimes = [];
         for iBin = 1:nBins
-            plot([meanTimes(iBin)-stdTimes(iBin) meanTimes(iBin)+stdTimes(iBin)],[binCenters(iBin) binCenters(iBin)],'-','color',repmat(0.8,[1,3]));
-            hold on;
+            theseTimes = all_Times(phaseCorr >= binEdges(iBin) & phaseCorr < binEdges(iBin+1));
+            meanTimes(iBin) = mean(theseTimes);
+            stdTimes(iBin) = std(theseTimes);
         end
-        plot(meanTimes,binCenters,'k-','lineWidth',2);
-        hold on;
-        plot(meanTimes,binCenters,'ko');
-        xticks([0.1:0.1:0.4]);
-        xlabel([doTiming,' (s)']);
-        yticks(binCenters);
-        yticklabels(num2str(binCenters(:),'%1.2f'));
-        ylabel('delta phase');
-        timeLabels{iSubplot} = num2str(timeCenters(ii),'%1.2f');
-        title({['t = ',timeLabels{iSubplot}],['p = ',num2str(pval),', rho = ',num2str(rho,2)]});
-        iSubplot = iSubplot + 1;
+        [rho,pval] = circ_corrcl(phaseCorr,all_Times);
+        event_meanTimes(ii,:) = meanTimes;
+    % %     if doPlot1
+    % %         all_pvals(iSubplot) = pval;
+    % %         all_rhos(iSubplot) = rho;
+    % %         subplot(rows,cols,iSubplot);
+    % %         % STD
+    % %         for iBin = 1:nBins
+    % %             plot([meanTimes(iBin)-stdTimes(iBin) meanTimes(iBin)+stdTimes(iBin)],[binCenters(iBin) binCenters(iBin)],'-','color',repmat(0.8,[1,3]));
+    % %             hold on;
+    % %         end
+    % %         plot(meanTimes,binCenters,'k-','lineWidth',2);
+    % %         hold on;
+    % %         plot(meanTimes,binCenters,'ko');
+    % %         xticks([0.1:0.1:0.4]);
+    % %         xlabel([doTiming,' (s)']);
+    % %         yticks(binCenters);
+    % %         yticklabels(num2str(binCenters(:),'%1.2f'));
+    % %         ylabel('delta phase');
+    % %         timeLabels{iSubplot} = num2str(timeCenters(ii),'%1.2f');
+    % %         title({['t = ',timeLabels{iSubplot}],['p = ',num2str(pval),', rho = ',num2str(rho,2)]});
+    % %         iSubplot = iSubplot + 1;
+    % %     end
     end
+    all_meanTimes{iEvent} = event_meanTimes;
 end
-if doPlot1
-    set(gcf,'color','w');
-    
-    figure;
-    yyaxis left;
-    plot(all_rhos);
-    xticks(1:numel(all_rhos));
-    xticklabels(timeLabels);
-    xtickangle(45);
-    ylim([0 0.5]);
-    yticks(ylim);
-    ylabel('rho');
-    yyaxis right;
-    plot(all_pvals);
-    ylim([0 0.1]);
-    yticks(ylim);
-    ylabel('pval');
-    
-    if doSave
-        saveFile = ['delta',doTiming,'corrSubplots_',eventFieldnames{iEvent},'.png'];
-        saveas(h,fullfile(savePath,saveFile));
-        close(h);
-    end
-end
+% % if doPlot1
+% %     set(gcf,'color','w');
+% %     
+% %     figure;
+% %     yyaxis left;
+% %     plot(all_rhos);
+% %     xticks(1:numel(all_rhos));
+% %     xticklabels(timeLabels);
+% %     xtickangle(45);
+% %     ylim([0 0.5]);
+% %     yticks(ylim);
+% %     ylabel('rho');
+% %     yyaxis right;
+% %     plot(all_pvals);
+% %     ylim([0 0.1]);
+% %     yticks(ylim);
+% %     ylabel('pval');
+% %     
+% %     if doSave
+% %         saveFile = ['delta',doTiming,'corrSubplots_',eventFieldnames{iEvent},'.png'];
+% %         saveas(h,fullfile(savePath,saveFile));
+% %         close(h);
+% %     end
+% % end
 
 if doPlot2
-    h = figuree(400,150);
-    imagesc(all_meanTimes');
-    colormap(gca,cmap);
-    xticks([1 round(n_timePoints/2) n_timePoints]);
-    xticklabels({'-1','0','1'});
-    xlabel('Time (s)');
-    yticks([1 ceil(nBins/2) nBins]);
-    yticklabels({'\pi','0','-\pi'});
-    title(['Delta Phase at ',eventFieldnames{iEvent}]);
-    caxis(caxisVals);
-    cbAside(gca,['mean ',doTiming],'k');
-    grid on;
+    h = figuree(1400,200);
+    for iEvent = 1:7
+        subplot(1,7,iEvent);
+        imagesc(all_meanTimes{iEvent}');
+        colormap(gca,cmap);
+        xticks([1 round(n_timePoints/2) n_timePoints]);
+        xticklabels({'-1','0','1'});
+        xlabel('Time (s)');
+        yticks([1 ceil(nBins/2) nBins]);
+        yticklabels({'\pi','0','-\pi'});
+        title({eventFieldnames{iEvent},'\delta phase'});
+        caxis(caxisVals);
+        if iEvent == 7
+            cbAside(gca,['mean ',doTiming],'k');
+        end
+        grid on;
 
-    [maxTime,maxIndex] = max(all_meanTimes(:));
-    [row_maxTime,col_maxTime] = ind2sub(size(all_meanTimes),maxIndex);
-    [minTime,minIndex] = min(all_meanTimes(:));
-    [row_minTime,col_minTime] = ind2sub(size(all_meanTimes),minIndex);
+        [maxTime,maxIndex] = max(all_meanTimes{iEvent}(:));
+        [row_maxTime,col_maxTime] = ind2sub(size(all_meanTimes{iEvent}),maxIndex);
+        [minTime,minIndex] = min(all_meanTimes{iEvent}(:));
+        [row_minTime,col_minTime] = ind2sub(size(all_meanTimes{iEvent}),minIndex);
 
-    hold on;
-    plot(row_minTime,col_minTime,'wo','markerSize',10);
-    text(row_minTime,col_minTime,['  min',doTiming],'color','w');
-    plot(row_maxTime,col_maxTime,'wo','markerSize',10);
-    text(row_maxTime,col_maxTime,['  max',doTiming],'color','w');
+        if false
+            hold on;
+            plot(row_minTime,col_minTime,'wo','markerSize',10);
+            text(row_minTime,col_minTime,['  min',doTiming],'color','w');
+            plot(row_maxTime,col_maxTime,'wo','markerSize',10);
+            text(row_maxTime,col_maxTime,['  max',doTiming],'color','w');
+        end
+    end
     set(gcf,'color','w');
     if doSave
-        saveFile = ['delta',doTiming,'corrMatrix_',eventFieldnames{iEvent},'.png'];
+        saveFile = ['delta',doTiming,'corrMatrix_allEvents_HILBERT.png'];
         saveas(h,fullfile(savePath,saveFile));
         close(h);
     end
--- ChoiceTask/LFPs/print/spikeLFPxcorrBySession.m
+++ ChoiceTask/LFPs/print/spikeLFPxcorrBySession.m
@@ -1,5 +1,6 @@
-doSetup = true;
+doSetup = false;
 doDebug = false;
+doPlot = false;
 nSurr = 100;
 zThresh = 2;
 freqList = logFreqList([1 200],10);
@@ -15,13 +16,13 @@ Wlength = 200;
 tWindow = 1;
 t = linspace(-tWindow,tWindow,Wlength);
 
-all_xcorrBands_events = [];
-all_surr_result = [];
 useUnitClass = 4;
 sessionCount = 0;
-for iSession = 1:2%numel(sessionNames)
+for iSession = 1:numel(sessionNames)
     if doSetup
         sessionTs = [];
+        all_xcorrBands_events = [];
+        all_surr_result = [];
         sessionUnits = find(ia == iSession)';
 % %         if numel(sessionUnits(primSec(sessionUnits,1) == useUnitClass)) < 3
 % %             disp(['skipping session: ',num2str(iSession)]);
@@ -180,104 +181,127 @@ for iSession = 1:2%numel(sessionNames)
     end
     % -- DEBUG END
 
-    h = figuree(1300,900);
-    rows = 4;
-    cols = 7;
-    for iEvent = 1:7
-        subplot(rows,cols,prc(cols,[1,iEvent]));
-        imagesc(t,1:numel(freqList),squeeze(mean(Wz_power(iEvent,:,:,:),3))');
-        colormap(gca,jet);
-        set(gca,'YDir','normal');
-        xlim([-1 1]);
-        xticks(sort([xlim 0]));
-        yticks(ytickIds);
-        yticklabels(ytickLabelText);
-        caxis([-5 5]);
-        if iEvent == 7
-            cb = cbAside(gca,'mean power','k');
-            cb.Ticks = caxis;
+    if doPlot
+        h = figuree(1300,900);
+        rows = 4;
+        cols = 7;
+        for iEvent = 1:7
+            subplot(rows,cols,prc(cols,[1,iEvent]));
+            imagesc(t,1:numel(freqList),squeeze(mean(Wz_power(iEvent,:,:,:),3))');
+            colormap(gca,jet);
+            set(gca,'YDir','normal');
+            xlim([-1 1]);
+            xticks(sort([xlim 0]));
+            yticks(ytickIds);
+            yticklabels(ytickLabelText);
+            caxis([-5 5]);
+            if iEvent == 7
+                cb = cbAside(gca,'mean power','k');
+                cb.Ticks = caxis;
+            end
+            primCount = sum(primSec(sessionUnits,1) == iEvent);
+            secCount = sum(primSec(sessionUnits,2) == iEvent);
+            if iEvent == 1
+                title({['s',num2str(iSession),', u',num2str(find(ia == iSession,1)),'-',num2str(find(ia == iSession,1,'last'))],...
+                    [eventFieldnames{iEvent}],[num2str(primCount),'^{primary}, ',num2str(secCount),'^{secondary}']});
+                ylabel('freq (Hz)');
+            else
+                title({'',[eventFieldnames{iEvent}],...
+                    [num2str(primCount),'^{primary}, ',num2str(secCount),'^{secondary}']});
+            end
+            grid on;
+
+            subplot(rows,cols,prc(cols,[2,iEvent]));
+            plot(t,mean(squeeze(all_SDEz(iEvent,:,:))),'k-','lineWidth',1);
+            xlim([-1 1]);
+            xticks(sort([xlim 0]));
+            ylim([-5 5]);
+            yticks(sort([ylim 0]));
+            if iEvent == 1
+                ylabel('mean SDE Z');
+            end
+            grid on;
+
+            subplot(rows,cols,prc(cols,[3,iEvent]));
+            imagesc(t_xcorr,1:numel(freqList),squeeze(xcorrBands_events(iEvent,:,:))');
+            colormap(gca,jet);
+            set(gca,'YDir','normal');
+            xlim([-1 1]);
+            xticks(sort([xlim 0]));
+            yticks(ytickIds);
+            yticklabels(ytickLabelText);
+            if iEvent == 1
+                ylabel('freq (Hz)');
+            end
+            caxis(round(minmaxRed(xcorrBands_events)));
+            if iEvent == 7
+                cb = cbAside(gca,'mean xcorr','k');
+                cb.Ticks = caxis;
+            end
+            grid on;
+
+            subplot(rows,cols,prc(cols,[4,iEvent]));
+            theseSurr = squeeze(surr_result(iEvent,:,:,:));
+            fracSurr = squeeze(sum(theseSurr)) ./ nSurr;
+            imagesc(t_xcorr,1:numel(freqList),fracSurr');
+            colormap(gca,cmap);
+            set(gca,'YDir','normal');
+            xlim([-1 1]);
+            xticks(sort([xlim 0]));
+            xlabel('time (s)');
+            yticks(ytickIds);
+            yticklabels(ytickLabelText);
+            if iEvent == 1
+                ylabel('freq (Hz)');
+            end
+            if iEvent == 7
+                cb = cbAside(gca,'shuffle sign.','k');
+                cb.Ticks = caxis;
+            end
+            caxis([-1 1]);
+            grid on;
         end
-        primCount = sum(primSec(sessionUnits,1) == iEvent);
-        secCount = sum(primSec(sessionUnits,2) == iEvent);
-        if iEvent == 1
-            title({['s',num2str(iSession),', u',num2str(find(ia == iSession,1)),'-',num2str(find(ia == iSession,1,'last'))],...
-                [eventFieldnames{iEvent}],[num2str(primCount),'^{primary}, ',num2str(secCount),'^{secondary}']});
-            ylabel('freq (Hz)');
-        else
-            title({'',[eventFieldnames{iEvent}],...
-                [num2str(primCount),'^{primary}, ',num2str(secCount),'^{secondary}']});
-        end
-        grid on;
-        
-        subplot(rows,cols,prc(cols,[2,iEvent]));
-        plot(t,mean(squeeze(all_SDEz(iEvent,:,:))),'k-','lineWidth',1);
-        xlim([-1 1]);
-        xticks(sort([xlim 0]));
-        ylim([-5 5]);
-        yticks(sort([ylim 0]));
-        if iEvent == 1
-            ylabel('mean SDE Z');
-        end
-        grid on;
-        
-        subplot(rows,cols,prc(cols,[3,iEvent]));
-        imagesc(t_xcorr,1:numel(freqList),squeeze(xcorrBands_events(iEvent,:,:))');
-        colormap(gca,jet);
-        set(gca,'YDir','normal');
-        xlim([-1 1]);
-        xticks(sort([xlim 0]));
-        yticks(ytickIds);
-        yticklabels(ytickLabelText);
-        if iEvent == 1
-            ylabel('freq (Hz)');
-        end
-        caxis(round(minmaxRed(xcorrBands_events)));
-        if iEvent == 7
-            cb = cbAside(gca,'mean xcorr','k');
-            cb.Ticks = caxis;
-        end
-        grid on;
-        
-        subplot(rows,cols,prc(cols,[4,iEvent]));
-        theseSurr = squeeze(surr_result(iEvent,:,:,:));
-        fracSurr = squeeze(sum(theseSurr)) ./ nSurr;
-        imagesc(t_xcorr,1:numel(freqList),fracSurr');
-        colormap(gca,cmap);
-        set(gca,'YDir','normal');
-        xlim([-1 1]);
-        xticks(sort([xlim 0]));
-        xlabel('time (s)');
-        yticks(ytickIds);
-        yticklabels(ytickLabelText);
-        if iEvent == 1
-            ylabel('freq (Hz)');
-        end
-        if iEvent == 7
-            cb = cbAside(gca,'shuffle sign.','k');
-            cb.Ticks = caxis;
-        end
-        caxis([-1 1]);
-        grid on;
+
+        set(gcf,'color','w');
+    %     savePath = '/Users/mattgaidica/Documents/Data/ChoiceTask/LFPs/perievent/xcorrBySession/_centerOut';
+        savePath = '/Users/mattgaidica/Documents/Data/ChoiceTask/LFPs/perievent/xcorrBySession';
+        saveFile = ['debug_u',num2str(find(ia == iSession,1),'%03d'),'-',num2str(find(ia == iSession,1,'last'),'%03d'),...
+            '_s',num2str(iSession,'%02d')];
+        saveas(h,fullfile(savePath,[saveFile,'.png']));
+        close(h);
     end
-    
-    set(gcf,'color','w');
-%     savePath = '/Users/mattgaidica/Documents/Data/ChoiceTask/LFPs/perievent/xcorrBySession/_centerOut';
-    savePath = '/Users/mattgaidica/Documents/Data/ChoiceTask/LFPs/perievent/xcorrBySession';
-    saveFile = ['debug_u',num2str(find(ia == iSession,1),'%03d'),'-',num2str(find(ia == iSession,1,'last'),'%03d'),...
-        '_s',num2str(iSession,'%02d')];
-    saveas(h,fullfile(savePath,[saveFile,'.png']));
-    close(h);
 end
 
 %% session-average plots
 rows = 2;
 cols = 7;
-figuree(1300,500);
+h = figuree(1300,500);
 
+xcorr_caxisVals = [];
+shuff_caxisVals = [];
 for iEvent = 1:7
+%     subplot(rows,cols,prc(cols,[1,iEvent]));
+%     imagesc(t_xcorr,1:numel(freqList),squeeze(mean(squeeze(Wz_power(iEvent,:,:,:)),2))');
+%     colormap(gca,jet);
+%     set(gca,'YDir','normal');
+%     xlim([-1 1]);
+%     xticks(sort([xlim 0]));
+%     yticks(ytickIds);
+%     yticklabels(ytickLabelText);
+%     if iEvent == 1
+%         ylabel('freq (Hz)');
+%     end
+%     caxis([-3 3]);
+%     if iEvent == 7
+%         cb = cbAside(gca,'mean Z-power','k');
+%         cb.Ticks = caxis;
+%     end
+%     title(eventFieldnames{iEvent});
+%     grid on;
+    
     subplot(rows,cols,prc(cols,[1,iEvent]));
-%     imagesc(t_xcorr,1:numel(freqList),squeeze(mean(squeeze( (:,iEvent,:,:))))');
-    imagesc(t_xcorr,1:numel(freqList),squeeze(mean(squeeze(Wz_power(iEvent,:,:,:)),2))');
+    imagesc(t_xcorr,1:numel(freqList),squeeze(mean(all_xcorrBands_events(:,iEvent,:,:),1))');
+    hold on;
     colormap(gca,jet);
     set(gca,'YDir','normal');
     xlim([-1 1]);
@@ -287,10 +311,8 @@ for iEvent = 1:7
     if iEvent == 1
         ylabel('freq (Hz)');
     end
-    caxis(round(minmaxRed(xcorrBands_events(4,:,:))));
-    if iEvent == 7
-        cb = cbAside(gca,'mean xcorr','k');
-        cb.Ticks = caxis;
+    if iEvent == 4
+        xcorr_caxisVals = caxis;
     end
     title(eventFieldnames{iEvent});
     grid on;
@@ -304,6 +326,7 @@ for iEvent = 1:7
     
     subplot(rows,cols,prc(cols,[2,iEvent]));
     imagesc(t_xcorr,1:numel(freqList),squeeze(mean(all_fracSurr))');
+    hold on;
     colormap(gca,cmap);
     set(gca,'YDir','normal');
     xlim([-1 1]);
@@ -311,16 +334,32 @@ for iEvent = 1:7
     xlabel('time (s)');
     yticks(ytickIds);
     yticklabels(ytickLabelText);
-    caxis([-0.5 0.5]);
     if iEvent == 1
         ylabel('freq (Hz)');
     end
-    if iEvent == 7
-        cb = cbAside(gca,'shuffle sign.','k');
-        cb.Ticks = caxis;
+    if iEvent == 4
+        shuff_caxisVals = caxis;
     end
     grid on;
 end
+for iEvent = 1:7
+    subplot(rows,cols,prc(cols,[1,iEvent]));
+    caxis(xcorr_caxisVals);
+    if iEvent == 7
+        cb = cbAside(gca,'mean xcorr','k');
+        cb.Ticks = caxis;
+    end
+    subplot(rows,cols,prc(cols,[2,iEvent]));
+    caxis(shuff_caxisVals);
+    if iEvent == 7
+        cb = cbAside(gca,'mean shuffle sign.','k');
+        cb.Ticks = caxis;
+    end
+end
+set(gcf,'color','w');
+savePath = '/Users/mattgaidica/Documents/Data/ChoiceTask/LFPs/perievent/xcorrBySession';
+saveas(h,fullfile(savePath,'allSessions_xcorrBySession.png'));
+close(h);
 %%
 
 function [refMean,refStd] = getRefsForZscore(tsPeths,tWindow,Wlength)
