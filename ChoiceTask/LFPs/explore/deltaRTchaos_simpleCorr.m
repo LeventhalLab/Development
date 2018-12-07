8977403c1972bc845bf33b2e2d01fb5d363f4c86
Matt Gaidica <matt@gaidi.ca>
2018-12-07 10:37:47 -0500

    Simple method working, producing matrices for power/phase/bands

--- ChoiceTask/LFPs/explore/deltaRTchaos_simpleCorr.m
+++ ChoiceTask/LFPs/explore/deltaRTchaos_simpleCorr.m
@@ -1,4 +1,4 @@
-doSetup = true;
+doSetup = false;
 zThresh = 5;
 tWindow = 1;
 freqList = {[1 4;4 8;13 30;30 70]};
@@ -41,40 +41,111 @@ end
 
 data_source = {trial_Wz_power trial_Wz_phase};
 [RTv,RTk] = sort(compiledRTs);
-rows = size(trial_Wz_power,4);
-useEvents = 1:7;
-cols = numel(useEvents);
-titleLabels = {'power','phase'};
-cmaps = {'jet','parula'};
 bandLabels = {'\delta','\theta','\beta','\gamma'};
-caxisVals = {[-2 5],[-pi pi]};
-for iCond = 1%1:2
-    ff(1400,800);
+titleLabels = {'power','phase'};
+
+% k1 = closest(RTv,0);
+% k2 = closest(RTv,1);
+% RTrange = k1:k2;
+pThresh = .001;
+intv = 100;
+useRange = intv:intv:numel(RTv);
+rows = 4;
+cols = 7;
+for iCond = 2%:2
+    ff(1500,800);
     useData = data_source{iCond};
-    for iEvent = useEvents
-        for iFreq = 1:size(trial_Wz_power,4)
+    for iEvent = 1:7
+        for iFreq = 1:4
+            thisData = squeeze(useData(iEvent,:,:,iFreq));
+            pMat = NaN(numel(useRange),numel(useRange));
+            range_log = [];
+            for iRange = 1:numel(useRange)-1
+                disp(iRange);
+                curRange = useRange(iRange);
+                for RTcenter = useRange(iRange:end-iRange)
+                    RTrange = (RTcenter - curRange + 1) : (RTcenter + curRange);
+                    range_log = [range_log numel(RTrange)];
+                    all_pval = [];
+                    all_rho = [];
+                    for iTime = 1:size(thisData,1)
+                        if iCond == 1
+                            [rho,pval] = corr(thisData(iTime,RTrange)',RTv(RTrange)');
+                        else
+                            [rho,pval] = circ_corrcl(thisData(iTime,RTrange)',RTv(RTrange)');
+                        end
+                        all_pval(iTime) = pval;
+                        all_rho(iTime) = rho;
+                    end
+                    pMat(find(useRange == curRange),find(useRange == RTcenter)) = sum(all_pval < pThresh);
+                end
+            end
+            % do plot
             subplot(rows,cols,prc(cols,[iFreq,iEvent]));
-            thisData = squeeze(useData(iEvent,:,RTk,iFreq));
-            imagesc(linspace(-tWindow,tWindow,size(thisData,1)),RTv,thisData');
-            colormap(gca,cmaps{iCond});
-            caxis(caxisVals{iCond});
-            if iFreq == 1
-                title({[eventFieldnames{iEvent},' - ',titleLabels{iCond}],bandLabels{iFreq}});
-            else
-                title(bandLabels{iFreq});
+            imagesc((100*pMat/numel(all_pval))');
+            colormap(jet);
+            caxis([0 100]);
+%             cb = colorbar;
+%             cb.Ticks = caxis;
+%             cb.Label.String = ['p < ',num2str(pThresh,'%1.3f'),' (% window)'];
+            xlabel('RT range (%)');
+            ylabel('RT center (s)');
+            
+            xInt = 11;
+            xtickVals = floor(linspace(0,numel(unique(range_log)),xInt));
+            xticks(xtickVals);
+            xtickLabelVals = linspace(0,100,xInt);
+            xticklabels(compose('%3.0f',xtickLabelVals));
+            xtickangle(270);
+            xlim([1 max(xtickVals)]);
+            
+            yInt = 11;
+            ytickVals = floor(linspace(1,numel(useRange),yInt));
+            yticks(ytickVals);
+            yticklabels(compose('%1.2f',RTv(useRange(ytickVals))));
+            set(gca,'ydir','normal');
+            title([bandLabels{iFreq},'-',titleLabels{iCond},' at ',eventFieldnames{iEvent}]);
+% %             set(gca,'fontSize',14);
+            drawnow;
+        end
+    end
+    set(gcf,'color','w');
+end
+
+if false
+    rows = 1;%size(trial_Wz_power,4);
+    useEvents = 1:7;
+    cols = numel(useEvents);
+    cmaps = {'jet','parula'};
+    caxisVals = {[-2 5],[-pi pi]};
+    for iCond = 2
+        ff(1400,800);
+        useData = data_source{iCond};
+        for iEvent = useEvents
+            for iFreq = 1%:size(trial_Wz_power,4)
+                subplot(rows,cols,prc(cols,[iFreq,iEvent]));
+                thisData = squeeze(useData(iEvent,:,RTk,iFreq));
+                imagesc(linspace(-tWindow,tWindow,size(thisData,1)),RTv,thisData');
+                colormap(gca,cmaps{iCond});
+                caxis(caxisVals{iCond});
+                if iFreq == 1
+                    title({[eventFieldnames{iEvent},' - ',titleLabels{iCond}],bandLabels{iFreq}});
+                else
+                    title(bandLabels{iFreq});
+                end
+                if iEvent == 1
+                    ylabel('RT (s)');
+                else
+                    yticklabels([]);
+                end
+                if iFreq == size(trial_Wz_power,4)
+                    xticks([-tWindow,0,tWindow]);
+                    xlabel('Time (s)');
+                else
+                    xticks([]);
+                end
+                grid on;
             end
-            if iEvent == 1
-                ylabel('RT (s)');
-            else
-                yticklabels([]);
-            end
-            if iFreq == size(trial_Wz_power,4)
-                xticks([-tWindow,0,tWindow]);
-                xlabel('Time (s)');
-            else
-                xticks([]);
-            end
-            grid on;
         end
     end
 end
