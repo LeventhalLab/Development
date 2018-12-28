39fdf3fb0b44a68e8d2ace515c5b8994a3b220fa
Matt Gaidica <matt@gaidi.ca>
2018-12-28 17:04:30 -0500

    Looking into spectrum peak at 10.7 Hz

--- ChoiceTask/LFPs/Figures/spectrum_MRL.m
+++ ChoiceTask/LFPs/Figures/spectrum_MRL.m
@@ -6,30 +6,76 @@ figPath = '/Users/mattgaidica/Box Sync/Leventhal Lab/Manuscripts/Mthal LFPs/Figu
 subplotMargins = [.05 .02];
 
 eventfields = {'Cue','Nose In','Tone','Nose Out','Side In','Side Out','Reward'};
-t = linspace(-1,1,size(scaloPower,2));
 scaloPower = squeeze(mean(session_Wz_power(:,:,:,:)));
 scaloPhase = squeeze(mean(session_Wz_phase(:,:,:,:)));
 scaloRayleigh = squeeze(mean(session_Wz_rayleigh_pval(:,:,:,:)));
+t = linspace(-1,1,size(scaloPower,2));
 
-h = ff(1200,450);
-rows = 1;
+h = ff(1400,800);
+rows = 2;
 cols = 7;
-colors = jet(30);
+colors = magma(30);
+hFreq = closest(freqList,9);
 for iEvent = 1:7
     for iFreq = 1:numel(freqList)
         subplot(rows,cols,prc(cols,[1,iEvent]));
-        plot(t,squeeze(scaloPower(iEvent,:,iFreq)),'color',colors(iFreq,:));
+        plot(t,smooth(squeeze(scaloPower(iEvent,:,iFreq))),'color',colors(iFreq,:));
         hold on;
     end
-    ylim([-1 1.5]);
+    plot(t,smooth(squeeze(scaloPower(iEvent,:,hFreq))),'color',colors(hFreq,:),'linewidth',3);
+    ylim([-1 4]);
+    yticks(sort([0 ylim]));
+    xticks(sort([0 xlim]));
     grid on;
-    title(eventfields{iEvent});
+    title({eventfields{iEvent},'power'},'color','w');
+    set(gca,'color','k');
+    set(gca,'XColor','w');
+    set(gca,'YColor','w');
+    if iEvent == 1
+        ylabel('Z-score','color','w');
+    end
+    if iEvent == 7
+        cb = colorbar;
+        colormap(colors);
+        cb.Limits = [0 1];
+        cb.Ticks = linspace(0,1,numel(freqList));
+        cb.TickLabels = compose('%2.1f',freqList);
+        cb.Color = 'w';
+    end
+    
+    for iFreq = 1:numel(freqList)
+        subplot(rows,cols,prc(cols,[2,iEvent]));
+        plot(t,smooth(squeeze(scaloPhase(iEvent,:,iFreq))),'color',colors(iFreq,:));
+        hold on;
+    end
+    plot(t,smooth(squeeze(scaloPhase(iEvent,:,hFreq))),'color',colors(hFreq,:),'linewidth',3);
+    ylim([0 0.6]);
+    yticks(ylim);
+    xticks(sort([0 xlim]));
+    grid on;
+    title('phase','color','w');
+    set(gca,'color','k');
+    set(gca,'XColor','w');
+    set(gca,'YColor','w');
+    if iEvent == 1
+        ylabel('MRL','color','w');
+    end
+    if iEvent == 7
+        cb = colorbar;
+        colormap(colors);
+        cb.Limits = [0 1];
+        cb.Ticks = linspace(0,1,numel(freqList));
+        cb.TickLabels = compose('%2.1f',freqList);
+        cb.Color = 'w';
+    end
+    
+    set(gcf,'color','k');
 end
 
 h = ff(1200,450);
 rows = 2;
 cols = 7;
-caxisVals = [-1 1];
+caxisVals = [-1 4];
 
 for iEvent = 1:7
     subplot_tight(rows,cols,prc(cols,[1,iEvent]),subplotMargins);
--- ChoiceTask/LFPs/utils/magma.m
+++ ChoiceTask/LFPs/utils/magma.m
@@ -1,3 +1,7 @@
-function cmap = magma
+function cmap = magma(varargin)
 cmapPath = '/Users/mattgaidica/Documents/MATLAB/LeventhalLab/Development/ChoiceTask/LFPs/utils/magma.png';
-cmap = mycmap(cmapPath);
+if ~isempty(varargin) % set nColors
+    cmap = mycmap(cmapPath,varargin{1});
+else
+    cmap = mycmap(cmapPath);
+end
