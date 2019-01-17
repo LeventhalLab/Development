af3175fcf07cd13a5539b8d36c4e9bc40adfd0be
Matt Gaidica <matt@gaidi.ca>
2019-01-17 16:15:49 -0500

    Need to run overnight on all units, then separate into dirSel groups

--- ChoiceTask/LFPs/explore/Ray_LFPspikeCorr.m
+++ ChoiceTask/LFPs/explore/Ray_LFPspikeCorr.m
@@ -1,29 +1,11 @@
-% need to build arrays of:
-% (1) norm. trial LFPs
-% (2) norm. unit firing
-% (3) unit-lfp lookup table
-% where, (1) has one entry per session,
-% (2) has multiple entries per session,
-% (3) implies (1)->has_many->(2)
-% for each event? only excited units?
-% close all;
 doSetup = false;
-
-doCompile = false;
-doPlot = true;
-doSave = true;
-
 if false
-    if doSetup
-        load('session_20181212_spikePhaseHist_NewSurrogates.mat', 'LFPfiles_local')
-        load('session_20181212_spikePhaseHist_NewSurrogates.mat', 'all_ts')
-        load('session_20181212_spikePhaseHist_NewSurrogates.mat', 'all_trials')
-    end
-    if doCompile
-        load('session_20180919_NakamuraMRL.mat','dirSelUnitIds','ndirSelUnitIds','primSec')
-        load('session_20181212_spikePhaseHist_NewSurrogates.mat', 'eventFieldnames')
-        load('Ray_LFPspikeCorr_setup.mat')
-    end
+    load('session_20181212_spikePhaseHist_NewSurrogates.mat', 'LFPfiles_local')
+    load('session_20181212_spikePhaseHist_NewSurrogates.mat', 'all_ts')
+    load('session_20181212_spikePhaseHist_NewSurrogates.mat', 'all_trials')
+    load('session_20180919_NakamuraMRL.mat','dirSelUnitIds','ndirSelUnitIds','primSec')
+    load('session_20181212_spikePhaseHist_NewSurrogates.mat', 'eventFieldnames')
+    load('Ray_LFPspikeCorr_setup.mat')
 end
 
 freqList = logFreqList([1 200],30);
@@ -76,11 +58,15 @@ if doSetup
     save('Ray_LFPspikeCorr_setup','all_Wz_power','all_zSDE','LFP_lookup','all_keepTrials','all_FR');
 end
 
-% [ ] try xcorr for comparison
+doCompile = true;
+doPlot = true;
+doSave = true;
+
 savePath = '/Users/mattgaidica/Documents/Data/ChoiceTask/LFPs/perievent/xcorrRayMethod';
 doDirSel = 1;
 nMs = 500;
 minFR = 10;
+nShuffle = 10;
 startIdx = round(Wlength/2) - round(nMs/2) + 1;
 LFP_range = startIdx:startIdx + nMs - 1;
 
@@ -95,281 +81,154 @@ else
     disp('all units');
     useUnits = FRunits;
 end
-useUnits = [187,188];
-[~,~,unitTrials,~] = cellfun(@size,all_Wz_power(LFP_lookup(useUnits)));
 
-if doCompile
-    lag_pval = NaN(nMs,7,numel(freqList));
-    lag_rho = NaN(size(lag_pval));
-    acors = NaN(sum(unitTrials),7,numel(freqList),nMs*2+1);
-    for iFreq = 1:numel(freqList)
-        disp(['Correlating ',num2str(freqList(iFreq),'%2.1f'),' Hz...']);
-        for iEvent = 1:7
-            disp(['  -> ',eventFieldnames{iEvent}]);
-            % xcorr method
-            neuronCount = 0;
-            unitTrialCount = 0;
-            for iNeuron = useUnits
-                neuronCount = neuronCount + 1;
-                for iTrial = 1:unitTrials(neuronCount)
-                    unitTrialCount = unitTrialCount + 1;
+all_shuff_pvals = NaN(numel(useUnits),7,numel(freqList),nMs*2+1);
+all_A_shuffled_mean = all_shuff_pvals;
+all_A = all_A_shuffled_mean;
+unitLookup = [];
+neuronCount = 0;
+for iNeuron = useUnits
+    neuronCount = neuronCount + 1;
+    unitLookup(neuronCount) = iNeuron;
+    disp(['iNeuron ',num2str(iNeuron,'%03d')]);
+    
+    if doCompile
+        acors = NaN(size(all_zSDE{iNeuron},1),7,numel(freqList),nMs*2+1);
+        for iTrial = 1:size(all_zSDE{iNeuron},1)
+            disp(['  -> trial',num2str(iTrial,'%03d')]);
+            for iEvent = 1:7
+                for iFreq = 1:numel(freqList)
                     Y = squeeze(all_Wz_power{LFP_lookup(iNeuron)}(iEvent,:,iTrial,iFreq));
                     X = squeeze(all_zSDE{iNeuron}(iTrial,iEvent,:))';
                     [acor,lag] = xcorr(X,Y,nMs);
-                    acors(unitTrialCount,iEvent,iFreq,:) = acor;
+                    acors(iTrial,iEvent,iFreq,:) = acor;
+                end
+            end
+        end
+        A = squeeze(mean(acors));
+        
+        acors = NaN(size(all_zSDE{iNeuron},1),7,numel(freqList),nMs*2+1);
+        A_shuffled = NaN(nShuffle,7,numel(freqList),nMs*2+1);
+        shuff_pvals_pos = zeros(size(A));
+        shuff_pvals_neg = zeros(size(A));
+        for iShuffle = 1:nShuffle
+            disp(['  -> shuffle',num2str(iShuffle,'%03d')]);
+            randTrial = randsample(1:size(all_zSDE{iNeuron},1),size(all_zSDE{iNeuron},1));
+            for iTrial = 1:size(all_zSDE{iNeuron},1)
+                for iEvent = 1:7
+                    for iFreq = 1:numel(freqList)
+                        Y = squeeze(all_Wz_power{LFP_lookup(iNeuron)}(iEvent,:,iTrial,iFreq));
+                        X = squeeze(all_zSDE{iNeuron}(randTrial(iTrial),iEvent,:))';
+                        [acor,lag] = xcorr(X,Y,nMs);
+                        acors(iTrial,iEvent,iFreq,:) = acor;
+                    end
+                end
+            end
+            B = squeeze(mean(acors)); % shuffled acor
+            A_shuffled(iShuffle,:,:,:) = B;
+            shuff_pvals_pos = shuff_pvals_pos + (A > B);
+            shuff_pvals_neg = shuff_pvals_neg + (B > A);
+        end
+        shuff_pvals = zeros(size(A));
+        shuff_pvals(shuff_pvals_pos > shuff_pvals_neg) = shuff_pvals_pos(shuff_pvals_pos > shuff_pvals_neg) / nShuffle;
+        shuff_pvals(shuff_pvals_neg > shuff_pvals_pos) = -1 * shuff_pvals_neg(shuff_pvals_neg > shuff_pvals_pos) / nShuffle;
+        
+        all_shuff_pvals(neuronCount,:,:,:) = shuff_pvals;
+        all_A(neuronCount,:,:,:) = A;
+        all_A_shuffled_mean(neuronCount,:,:,:) = squeeze(mean(A_shuffled));
+    end
+    
+
+    if doPlot
+        h = ff(1400,800);
+        left_color = [0 0 0];
+        right_color = [1 0 0];
+        set(h,'defaultAxesColorOrder',[left_color; right_color]);
+        rows = 4;
+        cols = 7;
+        acorCaxis = [-500 1500];
+        useData = {A,squeeze(mean(A_shuffled))};
+        t = linspace(-tWindow*1000,tWindow*1000,size(all_zSDE{1},3));
+        ySDE = [-3 3];
+        yLFP = [-6 6];
+        
+        for iEvent = 1:7
+            for iShuffle = 1:2
+                subplot(rows,cols,prc(cols,[iShuffle,iEvent]));
+                imagesc(lag,1:numel(freqList),squeeze(useData{iShuffle}(iEvent,:,:)));
+                hold on;
+                plot([0,0],ylim,'k:');
+                set(gca,'ydir','normal');
+                xlabel('spike lag (ms)');
+                yticks(linspace(min(ylim),max(ylim),numel(freqList)));
+                yticklabels(compose('%3.1f',freqList));
+                colormap(gca,jet);
+                caxis(acorCaxis);
+                ax = gca;
+                ax.YAxis.FontSize = 7;
+
+                if iShuffle == 1
+                    if iEvent == 1
+                        ylabel('Freq (Hz)');
+                        title({['u',num2str(iNeuron,'%03d')],eventFieldnames{iEvent},'xcorr'});
+                    else
+                        title({eventFieldnames{iEvent},'xcorr'});
+                    end
+                else
+                    title('xcorr_{shuffle}');
+                end
+                if iEvent == 7
+                    cbAside(gca,'acor','k');
                 end
             end
             
-% %             Y = NaN(sum(unitTrials)*nMs,1);
-% %             Yind = 1;
-% %             for iNeuron = useUnits
-% %                 A = squeeze(all_Wz_power{LFP_lookup(iNeuron)}(iEvent,LFP_range,:,iFreq));
-% %                 Y(Yind:Yind+numel(A)-1) = reshape(A',[numel(A) 1]);
-% %                 Yind = Yind+numel(A);
-% %             end
-% %             % [ ] frequency independent, could remove it from the iFreq loop
-% %             for iLag = 1:nMs
-% %                 X = NaN(size(Y));
-% %                 FR_range = LFP_range + (iLag - round(nMs/2)) - 1;
-% %                 Xind = 1;
-% %                 for iNeuron = useUnits
-% %                     A = squeeze(all_zSDE{iNeuron}(:,iEvent,FR_range));
-% %                     X(Xind:Xind+numel(A)-1) = reshape(A',[numel(A) 1]);
-% %                     Xind = Xind+numel(A);
-% %                 end
-% %                 [rho,pval] = corr(X,Y);
-% %                 lag_pval(iLag,iEvent,iFreq) = pval;
-% %                 lag_rho(iLag,iEvent,iFreq) = rho;
-% %             end
+            subplot(rows,cols,prc(cols,[3,iEvent]));
+            imagesc(lag,1:numel(freqList),squeeze(shuff_pvals(iEvent,:,:)));
+            hold on;
+            plot([0,0],ylim,'k:');
+            set(gca,'ydir','normal');
+            xlabel('spike lag (ms)');
+            yticks(linspace(min(ylim),max(ylim),numel(freqList)));
+            yticklabels(compose('%3.1f',freqList));
+            colormap(gca,jupiter);
+            caxis([-1 1]);
+            ax = gca;
+            ax.YAxis.FontSize = 7;
+            title(['shuff (x',num2str(nShuffle),')']);
+            if iEvent == 7
+                cbAside(gca,'pval','k');
+            end
+            
+            subplot(rows,cols,prc(cols,[4,iEvent]));
+            yyaxis left;
+            imagesc(t,1:numel(freqList),squeeze(mean(all_Wz_power{LFP_lookup(iNeuron)}(iEvent,:,:,:),3))');
+            set(gca,'ydir','normal');
+            colormap(gca,jet);
+            caxis(yLFP);
+            yticks(linspace(min(ylim),max(ylim),numel(freqList)));
+            yticklabels(compose('%3.1f',freqList));
+            ax = gca;
+            ax.YAxis(1).FontSize = 7;
+            if iEvent == 1
+                ylabel('Freq (Hz)');
+            end
+
+            yyaxis right;
+            plot(t,squeeze(mean(all_zSDE{iNeuron}(:,iEvent,:),1)),'lineWidth',2);
+            ylim(ySDE);
+            yticks(sort([0 ylim]));
+            if iEvent == 7
+                ylabel('SDE (Z)');
+            end
+            hold on;
+            plot([0,0],ylim,'k:');
+            xlabel('time (ms)');
+            title('LFP');
+        end
+        set(gcf,'color','w');
+        if doSave
+            saveas(h,fullfile(savePath,['u',num2str(iNeuron,'%03d'),'_ray_xcorr.png']));
+            close(h);
         end
     end
-    disp('Done compiling.');
-% %     save('session_20190116_Ray_ABlags_allUnits','A','B','acors','lag');
 end
-
-if doPlot
-    close all;
-    A = NaN(numel(useUnits),7,size(all_Wz_power{LFP_lookup(iNeuron)},2),numel(freqList));
-    B = NaN(numel(useUnits),7,size(all_Wz_power{LFP_lookup(iNeuron)},2));
-    for iEvent = 1:7
-        neuronCount = 0;
-        for iNeuron = useUnits
-            neuronCount = neuronCount + 1;
-            A(neuronCount,iEvent,:,:) = squeeze(mean(all_Wz_power{LFP_lookup(iNeuron)}(iEvent,:,:,:),3));
-            B(neuronCount,iEvent,:) = squeeze(mean(all_zSDE{iNeuron}(:,iEvent,:),1));
-        end
-    end
-    
-    h = ff(1400,800);
-    rows = 2;
-    cols = 7;
-    useData = {lag_rho,lag_pval};
-    useColormap = {'jet','hot'};
-% %     useCaxis = [-0.1,0.1;0 0.05];
-    useCaxis = [-500 1500;0 0.05];
-    useLabels = {'acor','pval'};
-    t = linspace(-tWindow*1000,tWindow*1000,size(all_zSDE{1},3));
-    tLag = linspace(-round(nMs/2),round(nMs/2),size(lag_rho,1));
-    lagLines = [min(tLag) min(tLag)];
-    ySDE = [-3 3];
-    yLFP = [-6 6];
-    iRow = 1;
-    for iEvent = 1:7
-        subplot(rows,cols,prc(cols,[iRow,iEvent]));
-        imagesc(lag,1:numel(freqList),squeeze(mean(acors(:,iEvent,:,:))));
-        hold on;
-        plot([0,0],ylim,'k:');
-        set(gca,'ydir','normal');
-% %         xticks([min(xlim) round(mean(xlim)) max(xlim)]);
-% %         xticklabels([min(tLag) 0 max(tLag)]);
-        xlabel('spike lag (ms)');
-        yticks(linspace(min(ylim),max(ylim),numel(freqList)));
-        yticklabels(compose('%3.1f',freqList));
-        colormap(gca,useColormap{iRow});
-        caxis(useCaxis(iRow,:));
-        title(eventFieldnames{iEvent});
-        if iEvent == 1
-            ylabel('Freq (Hz)');
-        end
-        if iEvent == 7
-            cbAside(gca,useLabels{iRow},'k');
-        end
-    end
-
-% %     for iRow = 1:2
-% %         for iEvent = 1:7
-% %             subplot(rows,cols,prc(cols,[iRow,iEvent]));
-% %             imagesc(squeeze(useData{iRow}(:,iEvent,:))');
-% %             set(gca,'ydir','normal');
-% %             xticks([min(xlim) round(mean(xlim)) max(xlim)]);
-% %             xticklabels([min(tLag) 0 max(tLag)]);
-% %             xlabel('spike lag (ms)');
-% %             yticks(linspace(min(ylim),max(ylim),numel(freqList)));
-% %             yticklabels(compose('%3.1f',freqList));
-% %             colormap(gca,useColormap{iRow});
-% %             caxis(useCaxis(iRow,:));
-% %             title(eventFieldnames{iEvent});
-% %             if iEvent == 1
-% %                 ylabel('Freq (Hz)');
-% %             end
-% %             if iEvent == 7
-% %                 cbAside(gca,useLabels{iRow},'k');
-% %             end
-% %         end
-% %     end
-    for iEvent = 1:7
-        subplot(rows,cols,prc(cols,[2,iEvent]));
-
-        yyaxis left;
-        imagesc(t,1:numel(freqList),squeeze(mean(A(:,iEvent,:,:)))');
-        set(gca,'ydir','normal');
-        colormap(gca,jet);
-        caxis(yLFP);
-        yticks(linspace(min(ylim),max(ylim),numel(freqList)));
-        yticklabels(compose('%3.1f',freqList));
-        if iEvent == 1
-            ylabel('Freq (Hz)');
-        end
-        
-        yyaxis right;
-        plot(t,squeeze(mean(B(:,iEvent,:))),'lineWidth',2);
-        ylim(ySDE);
-        yticks(sort([0 ylim]));
-        if iEvent == 7
-            ylabel('SDE (Z)');
-        end
-
-        hold on;
-% %         plot(-lagLines,ylim,'k:');
-% %         plot(lagLines,ylim,'k:');
-        plot([0,0],ylim,'k:');
-
-        xlabel('time (ms)');
-        title('LFP');
-    end
-    set(gcf,'color','w');
-end
-
-
-
-% % % % for iNeuron = 133%useUnits
-% % % %     if all_FR(iNeuron) < minFR
-% % % %         continue;
-% % % %     end
-% % % %     [~,~,unitTrials,~] = cellfun(@size,all_Wz_power(LFP_lookup(iNeuron)));
-% % % %     for iTrial = 1:unitTrials
-% % % %         if doCompile
-% % % %             lag_pval = [];
-% % % %             lag_rho = [];
-% % % %             for iFreq = 1:numel(freqList)
-% % % %                 disp(['Correlating ',num2str(freqList(iFreq),'%2.1f'),' Hz...']);
-% % % %                 for iEvent = 1:7
-% % % %                     disp(['  -> ',eventFieldnames{iEvent}]);
-% % % % % %                     Y = NaN(sum(unitTrials)*nMs,1);
-% % % %                     Y = NaN(nMs,1);
-% % % %                     Yind = 1;
-% % % %                     A = squeeze(all_Wz_power{LFP_lookup(iNeuron)}(iEvent,LFP_range,iTrial,iFreq));
-% % % %                     Y(Yind:Yind+numel(A)-1) = reshape(A',[numel(A) 1]);
-% % % %                     Yind = Yind+numel(A);
-% % % % 
-% % % %                     % [ ] this is frequency independent (i.e., redundant!)
-% % % %                     for iLag = 1:nMs+1
-% % % %                         X = NaN(size(Y));
-% % % %                         FR_range = LFP_range + (iLag - round(nMs/2)) - 1;
-% % % %                         Xind = 1;
-% % % %                         A = squeeze(all_zSDE{iNeuron}(iTrial,iEvent,FR_range));
-% % % %                         X(Xind:Xind+numel(A)-1) = reshape(A',[numel(A) 1]);
-% % % %                         Xind = Xind+numel(A);
-% % % % 
-% % % %                         [rho,pval] = corr(X,Y,'Type','Spearman');
-% % % %                         lag_pval(iLag,iEvent,iFreq) = pval;
-% % % %                         lag_rho(iLag,iEvent,iFreq) = rho;
-% % % %                     end
-% % % %                 end
-% % % %             end
-% % % %         end
-% % % % 
-% % % %         if doPlot
-% % % %             h = ff(1400,800);
-% % % %             left_color = [0 0 0];
-% % % %             right_color = [1 0 0];
-% % % %             set(h,'defaultAxesColorOrder',[left_color; right_color]);
-% % % %             rows = 3;
-% % % %             cols = 7;
-% % % %             useData = {lag_rho,lag_pval};
-% % % %             useColormap = {'jet','hot'};
-% % % %             useCaxis = [-1 1;0 0.05];
-% % % %             useLabels = {'rho','pval'};
-% % % %             t = linspace(-tWindow*1000,tWindow*1000,size(all_zSDE{1},3));
-% % % %             tLag = linspace(-round(nMs/2),round(nMs/2),size(lag_rho,1));
-% % % %             lagLines = [min(tLag) min(tLag)];
-% % % %             ySDE = [-3 3];
-% % % %             yLFP = [-6 6];
-% % % % 
-% % % %             for iRow = 1:2
-% % % %                 for iEvent = 1:7
-% % % %                     subplot(rows,cols,prc(cols,[iRow,iEvent]));
-% % % %                     imagesc(tLag,1:numel(freqList),squeeze(useData{iRow}(:,iEvent,:))');
-% % % %                     set(gca,'ydir','normal');
-% % % %                     xticks([min(xlim) round(mean(xlim)) max(xlim)]);
-% % % %                     xticklabels([min(tLag) 0 max(tLag)]);
-% % % %                     xlabel('spike lag (ms)');
-% % % %                     yticks(linspace(min(ylim),max(ylim),numel(freqList)));
-% % % %                     yticklabels(compose('%3.1f',freqList));
-% % % %                     colormap(gca,useColormap{iRow});
-% % % %                     caxis(useCaxis(iRow,:));
-% % % %                     if iRow == 1
-% % % %                         if iEvent == 1
-% % % %                             title({['u',num2str(iNeuron,'%03d')],eventFieldnames{iEvent},'xcorr'});
-% % % %                         else
-% % % %                             title({eventFieldnames{iEvent},'xcorr'});
-% % % %                         end
-% % % %                     end
-% % % %                     if iEvent == 1
-% % % %                         ylabel('Freq (Hz)');
-% % % %                     end
-% % % %                     if iEvent == 7
-% % % %                         cbAside(gca,useLabels{iRow},'k');
-% % % %                     end
-% % % %                 end
-% % % %             end
-% % % %             for iEvent = 1:7
-% % % %                 subplot(rows,cols,prc(cols,[3,iEvent]));
-% % % % 
-% % % %                 yyaxis left;
-% % % %                 A = squeeze(all_Wz_power{LFP_lookup(iNeuron)}(iEvent,:,iTrial,:));
-% % % %                 imagesc(t,1:numel(freqList),A');
-% % % %                 set(gca,'ydir','normal');
-% % % %                 colormap(gca,jet);
-% % % %                 caxis(yLFP);
-% % % %                 yticks(linspace(min(ylim),max(ylim),numel(freqList)));
-% % % %                 yticklabels(compose('%3.1f',freqList));
-% % % %                 if iEvent == 1
-% % % %                     ylabel('Freq (Hz)');
-% % % %                 end
-% % % % 
-% % % %                 yyaxis right;
-% % % %                 plot(t,squeeze(all_zSDE{iNeuron}(iTrial,iEvent,:)),'lineWidth',2);
-% % % %                 ylim(ySDE);
-% % % %                 yticks(sort([0 ylim]));
-% % % %                 if iEvent == 7
-% % % %                     ylabel('SDE (Z)');
-% % % %                 end
-% % % % 
-% % % %                 hold on;
-% % % %                 plot(-lagLines,ylim,'k:');
-% % % %                 plot(lagLines,ylim,'k:');
-% % % %                 plot([0,0],ylim,'k-');
-% % % % 
-% % % %                 xlabel('time (ms)');
-% % % %                 title('LFP');
-% % % %             end
-% % % % 
-% % % %             set(gcf,'color','w');
-% % % %             if doSave
-% % % %                 saveas(h,fullfile(savePath,['u',num2str(iNeuron,'%03d'),'_t',num2str(iTrial,'%03d'),'_xcorrRayMethod.png']));
-% % % %                 close(h);
-% % % %             end
-% % % %         end
-% % % %     end
-% % % % end
--- ChoiceTask/LFPs/run_code.m
+++ ChoiceTask/LFPs/run_code.m
@@ -1,9 +1,53 @@
 h = ff(1400,500);
-rows = 1;
+rows = 3;
 cols = 7;
-iTrial = 5;
-iFreq = 17;
+acorCaxis = [-500 1500];
+useData = {all_A,all_A_shuffled_mean};
+
 for iEvent = 1:7
-    subplot(rows,cols,prc(cols,[1,iEvent]));
-    imagesc(lag,1:numel(freqList),squeeze(acors(iTrial,iEvent,:,:)));
+    for iShuffle = 1:2
+        subplot(rows,cols,prc(cols,[iShuffle,iEvent]));
+        imagesc(lag,1:numel(freqList),squeeze(mean(useData{iShuffle}(:,iEvent,:,:))));
+        hold on;
+        plot([0,0],ylim,'k:');
+        set(gca,'ydir','normal');
+        xlabel('spike lag (ms)');
+        yticks(linspace(min(ylim),max(ylim),numel(freqList)));
+        yticklabels(compose('%3.1f',freqList));
+        colormap(gca,jet);
+        caxis(acorCaxis);
+        ax = gca;
+        ax.YAxis.FontSize = 7;
+        if iShuffle == 1
+            if iEvent == 1
+                ylabel('Freq (Hz)');
+                title({eventFieldnames{iEvent},'mean xcorr'});
+            else
+                title({eventFieldnames{iEvent},'mean xcorr'});
+            end
+        else
+            title('mean xcorr_{shuffle}');
+        end
+        if iEvent == 7
+            cbAside(gca,'acor','k');
+        end
+    end
+    
+    subplot(rows,cols,prc(cols,[3,iEvent]));
+    imagesc(lag,1:numel(freqList),squeeze(mean(all_shuff_pvals(:,iEvent,:,:))));
+    hold on;
+    plot([0,0],ylim,'k:');
+    set(gca,'ydir','normal');
+    xlabel('spike lag (ms)');
+    yticks(linspace(min(ylim),max(ylim),numel(freqList)));
+    yticklabels(compose('%3.1f',freqList));
+    colormap(gca,jupiter);
+    caxis([-1 1]);
+    ax = gca;
+    ax.YAxis.FontSize = 7;
+    title(['shuff (x',num2str(nShuffle),')']);
+    if iEvent == 7
+        cbAside(gca,'pval','k');
+    end
 end
+set(gcf,'color','w');
