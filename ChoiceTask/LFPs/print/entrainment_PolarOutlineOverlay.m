192d43346ac0c96c31f0602d59eadf102d85d992
Matt Gaidica <matt@gaidi.ca>
2019-02-20 14:56:59 -0500

    Entrainment overlay in polar axes is kind of a mess

--- ChoiceTask/LFPs/print/entrainment_PolarOutlineOverlay.m
+++ ChoiceTask/LFPs/print/entrainment_PolarOutlineOverlay.m
@@ -8,81 +8,60 @@ pThresh = 1; % 0.05;
 dirSelRanges = {[1:366],dirSelUnitIds,ndirSelUnitIds};
 dirSelTypes = {'all','dirSel','ndirSel'};
 trialTypes = {'shuffle','In-trial','Inter-trial'};
-barData = [];
-meanZ = [];
-unitCount = [];
 iFreq = 6;
-rows = 3;
+rows = 1;
 cols = 3;
+nBins = 12;
+binEdges = linspace(-pi,pi,nBins + 1);
 
-zlimVals = [0.05 0.1];
-caxisVals = [0.05 0.1];
-flatylimVals = [0.075 0.09];
-zscoreLabel = 'binfrac';
+condLabels_wCount = {['allUnits (n = ',num2str(numel(dirSelRanges{1})),')'],...
+    ['dirSel (n = ',num2str(numel(dirSelRanges{2})),')'],...
+    ['ndirSel (n = ',num2str(numel(dirSelRanges{3})),')']};
  
-useylims = [0.5 24.5];
-ytickVals = [1 24];
-for iDirSel = 1:3
-    h = ff(1400,800);
-    for iTrialType = 1:3
+h = ff(1400,500);
+for iTrialType = 1:3
+    for iDirSel = 1:3
         use_pvals = conds_pvals{iTrialType}(dirSelRanges{iDirSel},iFreq);
         use_angles = conds_angles{iTrialType}(dirSelRanges{iDirSel},:,iFreq);
         sigMat = use_angles(use_pvals < pThresh,:);
-        
         subplot(rows,cols,prc(cols,[1,iTrialType]));
-        bar3color([Zdata;Zdata]);
-        view(140,50);
-        zlim(zlimVals);
-        title([dirSelTypes{iDirSel},', ',trialTypes{iTrialType}]);
-        ylim(useylims);
-        caxis(caxisVals);
-        yticks([0.5 12.5 24.5]);
-        yticklabels([0 360 720]);
-        xlim(usexlims);
-        xticks(xtickVals);
-        xlabel('units');
         
-        subplot(rows,cols,prc(cols,[2,iTrialType]));
-        bar3color([Zdata;Zdata]);
-        view(0,90);
-        zlim(zlimVals);
-        title([dirSelTypes{iDirSel},', ',trialTypes{iTrialType}]);
-        ylim(useylims);
-        caxis(caxisVals);
-        yticks([0.5 12.5 24.5]);
-        yticklabels([0 360 720]);
-        xlim(usexlims);
-        xticks(xtickVals);
-        xlabel('units');
-        
-        subplot(rows,cols,prc(cols,[3,iTrialType]));
-        yyaxis left;
-        plot(meanZ,'linewidth',3);
-        if useZscore
-            ylabel('mean Z');
-        else
-            ylabel('frac of bins');
+        % order, sort
+        Z = sigMat ./ sum(sigMat,2);
+        [~,kZ] = sort(max(Z'));
+        Z = Z(kZ,:);
+        kBins = [];
+        for iNeuron = 1:size(Z,1)
+            [~,k] = max(Z(iNeuron,:));
+            kBins(iNeuron) = k;
         end
-        ylim(flatylimVals);
-        yticks(unique(sort([0 ylim])));
-        yyaxis right;
-        plot([maxHist maxHist],'linewidth',3);
-        ylabel('frac of units');
-        xlim([1 24]);
-        xticks([1 12.5 24]);
-        xticklabels([0 360 720]);
-        title('flattened');
-        ylim([0 0.3]);
-        yticks(ylim);
-        grid on;
+        [~,k] = sort(kBins);
+        Z = Z(k,:);
         
-        colormap(jet);
+        colors = [0 0 0;lines(2)];
+        hp = polarhistogram('BinEdges',binEdges,'BinCounts',mean(Z));
+        hp.DisplayStyle = 'stairs';
+        hold on;
+        rlim([0.09 .101]);
+        hp.LineWidth = 4;
+        rticks(rlim);
+        hp.EdgeColor = colors(iDirSel,:);
+        
+% %         colors = jet(size(sigMat,1));
+% %         for iNeuron = 1:size(sigMat,1)
+% %             hp = polarhistogram('BinEdges',binEdges,'BinCounts',Z(iNeuron,:));
+% %             hp.DisplayStyle = 'stairs';
+% %             hp.EdgeColor = colors(iNeuron,:);
+% %             hold on;
+% %             drawnow;
+% %         end
+        title([trialTypes{iTrialType}]);
     end
-    set(gcf,'color','w');
-    addNote(h,[num2str(freqList(iFreq),'%1.2f'),' Hz'],20);
-    if doSave
-        saveas(h,fullfile(savePath,['3D_entrainment_',zscoreLabel,'_',trialTypes{iTrialType},'_',...
-            dirSelTypes{iDirSel},'_f',num2str(iFreq,'%02d'),'.png']));
-        close(h);
-    end
+    legend(condLabels_wCount,'location','southoutside');
+end
+set(gcf,'color','w');
+addNote(h,[num2str(freqList(iFreq),'%1.2f'),' Hz'],20);
+if doSave
+    saveas(h,fullfile(savePath,['polarOverlay_entrainment_f',num2str(iFreq,'%02d'),'.png']));
+    close(h);
 end
