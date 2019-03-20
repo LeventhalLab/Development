2e5cdb85841732672972a601c45fdc1d35a71b71
Matt Gaidica <matt@gaidi.ca>
2019-03-20 16:25:02 -0400

    Back to simple method

--- ChoiceTask/LFPs/utils/compileTrialTimeRanges.m
+++ ChoiceTask/LFPs/utils/compileTrialTimeRanges.m
@@ -1,10 +1,7 @@
-function [intrialTimeRanges,intertrialTimeRanges] = compileTrialTimeRanges(trials,maxTrialTime)
-% intrial will start with first trial
-% intertrial will start from 0 to first trial and end at the start of the last trial
-% trials must be given in trial-order
-
-intrialTimeRanges = NaN(numel(trials),2);
-intertrialTimeRanges = intrialTimeRanges;
+function trialTimeRanges = compileTrialTimeRanges(trials)
+maxTrialTime = 20;
+trialTimeRanges = [];
+trialCount = 0;
 for iTrial = 1:numel(trials)
     fields = fieldnames(trials(iTrial).timestamps);
     trialTimes = [];
@@ -14,20 +11,8 @@ for iTrial = 1:numel(trials)
             trialTimes = [trialTimes ts];
         end
     end
-    
-    intrialTimeRanges(iTrial,:) = [min(trialTimes),max(trialTimes)];
-    if iTrial == 1
-        intertrialTimeRanges(iTrial,:) = [0 intrialTimeRanges(iTrial,1)];
-    else
-        intertrialTimeRanges(iTrial,:) = [intrialTimeRanges(iTrial-1,2),intrialTimeRanges(iTrial,1)];
-        if sign(diff(intertrialTimeRanges(iTrial,:))) == -1
-            disp('stop');
-        end
-        if max(trialTimes) < max(intrialTimeRanges(:,2))
-            error('trials need to be in trial-order');
-        end
+    if max(trialTimes) - min(trialTimes) < maxTrialTime
+        trialCount = trialCount + 1;
+        trialTimeRanges(trialCount,:) = [min(trialTimes),max(trialTimes)];
     end
 end
-
-longTrials = find(diff(intrialTimeRanges,1,2) > maxTrialTime);
-intrialTimeRanges(longTrials,:) = repmat([NaN NaN],size(longTrials));
