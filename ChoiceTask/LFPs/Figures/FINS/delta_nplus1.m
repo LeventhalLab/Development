f4588d1ff6219229f3d93e7acf5fa0235932a9fd
Matt Gaidica <matt@gaidi.ca>
2020-06-26 18:10:51 -0400

    final form n-1

--- ChoiceTask/LFPs/Figures/FINS/delta_nplus1.m
+++ ChoiceTask/LFPs/Figures/FINS/delta_nplus1.m
@@ -41,117 +41,109 @@ if doSetup
     end
 end
 
-totalTrials = 0;
-totalRts = 0;
-for iSession = 1%:numel(WRT)
-    totalTrials = totalTrials + size(WRT(iSession).Wz_power,3);
-    totalRts = totalRts + numel(WRT(iSession).trialIds);
-end
+% % totalTrials = 0;
+% % totalRts = 0;
+% % for iSession = 1%:numel(WRT)
+% %     totalTrials = totalTrials + size(WRT(iSession).Wz_power,3);
+% %     totalRts = totalRts + numel(WRT(iSession).trialIds);
+% % end
 
-delta_corr = zeros(numel(eventNames),totalTrials);
-rt_corr = zeros(1,totalRts);
-curPos_delta = 1;
-curPos_rt = 1;
-for iEvent = 1:numel(eventNames)
-    for iSession = 1%:numel(WRT)
+close all
+ff(1200,400);
+rows = 1;
+cols = 3;
+iEvent = 1;
+n = 1;
+op = 0.15;
+prettyNames = {'Cue','Nose Out'};
+mean_data = [];
+lns = [];
+
+for iSession = 1:numel(WRT)
+    disp(iSession);
+    delta_corr = zeros(numel(eventNames),size(WRT(iSession).Wz_power,3));
+    rt_corr = zeros(1,numel(WRT(iSession).trialIds));
+    curPos_delta = 1;
+    curPos_rt = 1;
+    for iEvent = 1:numel(eventNames)
+        
         thisPower = mean(squeeze(WRT(iSession).Wz_power(iEvent,:,:)));
-        delta_corr(iEvent,curPos_delta:curPos_delta+numel(thisPower)-1) = thisPower;
+        delta_corr(iEvent,:) = thisPower;
         
         if iEvent == 1
             theseTrials = WRT(iSession).trialIds;
             [v,k] = sort(theseTrials);
             theseRT = WRT(iSession).allTimes;
             theseRT = theseRT(k);
-            rt_corr(1,curPos_rt:curPos_rt+numel(theseRT)-1) = theseRT;
+            rt_corr(1,:) = theseRT;
         end
-        curPos_delta = curPos_delta + numel(thisPower);
-        curPos_rt = curPos_rt + numel(theseRT);
     end
-    curPos_delta = 1;
-    curPos_rt = 1;
-end
-
-nR = 10;
-r_plot = zeros(3,nR);
-p_plot = zeros(3,nR);
-for n = 0:nR
-    for iEvent = 1:numel(eventNames)
-        data = squeeze(delta_corr(iEvent,:));
-        [data_rm,Ix] = rmoutliers(data);
+    
+    nR = 10;
+    r_plot = zeros(3,nR);
+    p_plot = zeros(3,nR);
+    for n = 1:nR
+        for iEvent = 1:numel(eventNames)
+            data = squeeze(delta_corr(iEvent,:));
+            [data_rm,Ix] = rmoutliers(data);
+            x = data_rm(1:end-n)';
+            y = data_rm(n+1:end)';
+            [r,p] = corr(x,y);
+            r_plot(iEvent,n) = r;
+            p_plot(iEvent,n) = p;
+        end
+    end
+    
+    for n = 1:nR
+        data = squeeze(rt_corr(1,:));
+        data_rm = data(data > 0);
         x = data_rm(1:end-n)';
         y = data_rm(n+1:end)';
         [r,p] = corr(x,y);
-        r_plot(iEvent,n+1) = r;
-        p_plot(iEvent,n+1) = p;
+        r_plot(3,n) = r;
+        p_plot(3,n) = p;
     end
-end
-
-for n = 0:nR
-    data = squeeze(rt_corr(1,:));
-    data_rm = data(data > 0);
-    x = data_rm(1:end-n)';
-    y = data_rm(n+1:end)';
-    [r,p] = corr(x,y);
-    r_plot(3,n+1) = r;
-    p_plot(3,n+1) = p;
-end
-
-close all
-ff(1200,400);
-rows = 2;
-cols = numel(eventNames)+1;
-n = 1;
-for iEvent = 1:numel(eventNames)
-    data = squeeze(delta_corr(iEvent,:));
-    [data_rm,Ix] = rmoutliers(data);
-    x = data_rm(1:end-n)';
-    y = data_rm(n+1:end)';
     
-    subplot(rows,cols,prc(cols,[1,iEvent]));
-    f = fit(x,y,'poly1');
-    plot(f,x,y,'k.');
-    [r,p] = corr(x,y);
-%     xlim([0,20]);
-%     ylim(xlim);
-    title({eventNames{iEvent},sprintf('p = %1.2e, r = %1.2f',p,r)});
-    xlabel('\delta power trial_n');
-    ylabel(['\delta power trial_{n+',num2str(n),'}']);
+    t = fliplr(-(1:nR));
+    for iEvent = 1:numel(eventNames)
+        subplot(rows,cols,prc(cols,[1,iEvent]));
+        plot(t,r_plot(iEvent,-t),'-','linewidth',1,'color',[0 0 0 op]);
+        mean_data(iEvent,iSession,:) = r_plot(iEvent,-t);
+        hold on;
+        ps = p_plot(iEvent,:);
+        plot(xlim,[0,0],':','color',[0 0 0 0.3]);
+        plot(-find(ps < 0.05),r_plot(iEvent,ps < 0.05),'*','color',[1 0 0 op]);
+        title(prettyNames{iEvent});
+        xlabel('Trials back');
+        ylabel('Corr. Coeff');
+        xlim([min(t) max(t)]);
+        xticks(t);
+        ylim([-1 1]);
+        set(gca,'fontsize',14);
+    end
     
-    subplot(rows,cols,prc(cols,[2,iEvent]));
-    plot(r_plot(iEvent,:),'k-','linewidth',2);
+    subplot(rows,cols,prc(cols,[1,3]));
+    lns(1) = plot(t,r_plot(3,-t),'-','linewidth',1,'color',[0 0 0 op]);
+    mean_data(3,iSession,:) = r_plot(3,-t);
     hold on;
-    ps = p_plot(iEvent,:);
-    ln1 = plot(find(ps < 0.05),r_plot(iEvent,ps < 0.05),'r*');
-    title('Corr. Coeff (r)');
-    xlabel('trial_n x trial_{n+x}');
-    ylabel('r');
-    xlim([0 numel(r_plot(iEvent,:))]);
-    ylim([0 1]);
-    legend(ln1,'p < 0.05')
+    ps = p_plot(3,:);
+    plot(xlim,[0,0],':','color',[0 0 0 0.3]);
+    if sum(ps < 0.05) > 0
+        lns(3) = plot(-find(ps < 0.05),r_plot(3,ps < 0.05),'*','color',[1 0 0 op]);
+    end
+    title('Reaction Time');
+    xlabel('Trials back');
+    xticks(t);
+    ylabel('Corr. Coeff');
+    xlim([min(t) max(t)]);
+    ylim([-1 1]);
+    set(gca,'fontsize',14);
+    drawnow;
 end
 
-data = squeeze(rt_corr(1,:));
-data_rm = data(data > 0);
-x = data_rm(1:end-n)';
-y = data_rm(n+1:end)';
-subplot(rows,cols,prc(cols,[1,3]));
-f = fit(x,y,'poly1');
-plot(f,x,y,'k.');
-[r,p] = corr(x,y);
-xlim([0,0.8]);
-ylim(xlim);
-title({'Trial RT',sprintf('p = %1.2e, r = %1.2f',p,r)});
-xlabel('RT (s) trial_n');
-ylabel(['RT (s) trial_{n+',num2str(n),'}']);
-
-subplot(rows,cols,prc(cols,[2,3]));
-plot(r_plot(3,:),'k-','linewidth',2);
-hold on;
-ps = p_plot(3,:);
-ln1 = plot(find(ps < 0.05),r_plot(3,ps < 0.05),'r*');
-title('Corr. Coeff (r)');
-xlabel('trial_n x trial_{n+x}');
-ylabel('r');
-xlim([1 numel(r_plot(iEvent,:))]);
-ylim([0 0.5]);
-legend(ln1,'p < 0.05');
+for ii = 1:3
+    subplot(rows,cols,ii);
+    lns(2) = plot(t,squeeze(mean(mean_data(ii,:,:))),'k-','linewidth',2);
+end
+legend(lns,{'Single Session','mean(All Sessions)','p < 0.05'},'location','northwest');
+legend box off;
